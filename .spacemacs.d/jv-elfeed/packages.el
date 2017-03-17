ac6d21aef0c0747a6d75f5e4878eea326e7bcf16
Julio Cesar Villasante <jvillasantegomez@gmail.com>
2017-03-17 12:27:59 -0400

    some more changes

--- .spacemacs.d/jv-elfeed/packages.el
+++ .spacemacs.d/jv-elfeed/packages.el
@@ -3,8 +3,20 @@
 
 (defun jv-elfeed/post-init-elfeed ()
   (with-eval-after-load 'elfeed
+    ;; elfeed db path
     (setq elfeed-db-directory (concat my-dropbox-path "/Personal/elfeed/elfeed_db"))
 
+    ;; load and save db on open and quit
     (spacemacs/set-leader-keys "af" 'jv/elfeed-load-db-and-open)
     (evil-define-key 'evilified elfeed-search-mode-map "q" 'jv/elfeed-save-db-and-bury)
-    (evil-define-key 'evilified elfeed-search-mode-map "Q" 'jv/elfeed-save-db-and-bury)))
+
+    ;; face for starred articles
+    (defface elfeed-search-starred-title-face
+      '((t :foreground "#f77"))
+      "Marks a starred Elfeed entry.")
+    (push '(starred elfeed-search-starred-title-face) elfeed-search-face-alist)
+
+    ;; starred articles
+    (evil-define-key 'evilified elfeed-search-mode-map "*" 'jv/elfeed-star)
+    (evil-define-key 'evilified elfeed-search-mode-map "8" 'jv/elfeed-unstar)
+    (evil-define-key 'evilified elfeed-search-mode-map "S" 'jv/elfeed-show-starred)))
