cadfff0aafea9bc3316ad9f0b95cc2c7bc79c666
Julio Cesar Villasante <jvillasantegomez@gmail.com>
2017-03-18 08:33:37 -0400

    take out elfeed star/unstar

--- .spacemacs.d/jv-elfeed/funcs.el
+++ .spacemacs.d/jv-elfeed/funcs.el
@@ -18,33 +18,3 @@
   (interactive)
   (elfeed-db-save)
   (quit-window))
-
-;; code to add and remove a starred tag to elfeed article
-;; based on http://matt.hackinghistory.ca/2015/11/22/elfeed/
-
-;; add a star
-(defun jv/elfeed-star ()
-  "Apply starred to all selected entries."
-  (interactive )
-  (let* ((entries (elfeed-search-selected))
-         (tag (intern "starred")))
-
-    (cl-loop for entry in entries do (elfeed-tag entry tag))
-    (mapc #'elfeed-search-update-entry entries)
-    (unless (use-region-p) (forward-line))))
-
-;; remove a start
-(defun jv/elfeed-unstar ()
-  "Remove starred tag from all selected entries."
-  (interactive )
-  (let* ((entries (elfeed-search-selected))
-         (tag (intern "starred")))
-
-    (cl-loop for entry in entries do (elfeed-untag entry tag))
-    (mapc #'elfeed-search-update-entry entries)
-    (unless (use-region-p) (forward-line))))
-
-;; shortcut to jump to starred bookmark
-(defun jv/elfeed-show-starred ()
-  (interactive)
-  (bookmark-jump "elfeed-starred"))
