d0f3ce6c3053c4cb5c5f2574ff5f144f0ab49cae
Julio Cesar Villasante <jvillasantegomez@gmail.com>
2017-08-13 15:06:28 -0400

    Common lisp stuff

--- .spacemacs.d/layers/jv-lisp/config.el
+++ .spacemacs.d/layers/jv-lisp/config.el
@@ -1,7 +1,9 @@
 ;;; config.el --- jv-lisp Layer config File for Spacemacs
-(setf slime-default-lisp 'sbcl)
-(setq slime-net-coding-system 'utf-8-unix)
+(load (expand-file-name "~/.roswell/helper.el"))
 
 (setf slime-lisp-implementations
   `((roswell ("ros" "-Q" "run"))
      (sbcl ("ros" "-L" "sbcl" "+R" "-l" "~/.sbclrc" "run"))))
+
+(setf slime-default-lisp 'sbcl)
+(setq slime-net-coding-system 'utf-8-unix)
