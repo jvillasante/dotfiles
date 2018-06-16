49fde37d82a03e3fae311184191454502ae0cd38
Julio Cesar Villasante <jvillasantegomez@gmail.com>
2018-06-16 08:53:29 -0400

    No RTags for now

--- .spacemacs.d/layers/jv-cpp-common/packages.el
+++ .spacemacs.d/layers/jv-cpp-common/packages.el
@@ -2,7 +2,8 @@
   '(cc-mode
      company
      modern-cpp-font-lock
-     rtags))
+     ;; rtags
+     ))
 
 (defun jv-cpp-common/post-init-cc-mode ()
   (add-hook 'c++-mode-hook
@@ -24,37 +25,37 @@
       (modern-c++-font-lock-global-mode t)
       (spacemacs|diminish modern-c++-font-lock-mode " ⊗" " x"))))
 
-(defun jv-cpp-common/init-rtags ()
-  (use-package rtags
-    :defer f
-    :init
-    (progn
-      (spacemacs/set-leader-keys-for-major-mode 'c++-mode
-        "d" 'rtags-find-symbol-at-point
-        "D" 'jv-cpp-common/rtags-find-symbol-at-point-other-file
-        "/" 'rtags-find-symbol
-        "R" 'rtags-find-references-at-point
-        ;; "r" 'rtags-find-references-at-point-in-file to implement
-        "v" 'rtags-find-virtuals-at-point
-        "i" 'rtags-imenu
-        "C-r" 'rtags-rename-symbol
+;; (defun jv-cpp-common/init-rtags ()
+;;   (use-package rtags
+;;     :defer f
+;;     :init
+;;     (progn
+;;       (spacemacs/set-leader-keys-for-major-mode 'c++-mode
+;;         "d" 'rtags-find-symbol-at-point
+;;         "D" 'jv-cpp-common/rtags-find-symbol-at-point-other-file
+;;         "/" 'rtags-find-symbol
+;;         "R" 'rtags-find-references-at-point
+;;         ;; "r" 'rtags-find-references-at-point-in-file to implement
+;;         "v" 'rtags-find-virtuals-at-point
+;;         "i" 'rtags-imenu
+;;         "C-r" 'rtags-rename-symbol
 
-        ;; print prefix
-        "pt" 'rtags-print-class-hierarchy
-        "pe" 'rtags-print-enum-value-at-point
-        "pi" 'rtags-print-dependencies
-        "ps" 'rtags-print-symbol-info
-        "pp" 'rtags-preprocess-file
+;;         ;; print prefix
+;;         "pt" 'rtags-print-class-hierarchy
+;;         "pe" 'rtags-print-enum-value-at-point
+;;         "pi" 'rtags-print-dependencies
+;;         "ps" 'rtags-print-symbol-info
+;;         "pp" 'rtags-preprocess-file
 
-        ;; TODO: planned micro state
-        ;; "o" (rtags-occurence-transient state)
-        ;; "n" 'rtags-next-match
-        ;; "N/p" 'rtags-previous-match
-        )
-      (add-hook 'rtags-jump-hook 'evil-set-jump)
-      (add-to-list 'spacemacs-jump-handlers-c++-mode '(rtags-find-symbol-at-point :async t)))
-    :config
-    (progn
-      (require 'rtags-helm)
-      (setq rtags-jump-to-first-match nil)
-      (setq rtags-use-helm t))))
+;;         ;; TODO: planned micro state
+;;         ;; "o" (rtags-occurence-transient state)
+;;         ;; "n" 'rtags-next-match
+;;         ;; "N/p" 'rtags-previous-match
+;;         )
+;;       (add-hook 'rtags-jump-hook 'evil-set-jump)
+;;       (add-to-list 'spacemacs-jump-handlers-c++-mode '(rtags-find-symbol-at-point :async t)))
+;;     :config
+;;     (progn
+;;       (require 'rtags-helm)
+;;       (setq rtags-jump-to-first-match nil)
+;;       (setq rtags-use-helm t))))
--- .spacemacs.d/layers/jv/packages.el
+++ .spacemacs.d/layers/jv/packages.el
@@ -6,9 +6,7 @@
      flyspell-lazy
      password-store
      pass
-     crux
-     ;; adafruit-wisdom
-     ))
+     crux))
 
 (defun jv/init-editorconfig ()
   (use-package editorconfig
@@ -83,9 +81,3 @@
   (use-package crux
     :ensure t
     :bind (("C-c o" . crux-open-with))))
-
-;; (defun jv/init-adafruit-wisdom ()
-;;   (use-package adafruit-wisdom
-;;     :config
-;;     (setq inhibit-startup-message t)
-;;     (spacemacs-buffer/append (concat "\n" ";; " (adafruit-wisdom-select) "\n"))))
