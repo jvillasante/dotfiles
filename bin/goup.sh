d00575e40ac65107673db4e818c296cd2ac86562
Julio Cesar Villasante <jvillasantegomez@gmail.com>
2018-08-28 20:35:34 -0400

    Updates goup for new gocode package

--- bin/goup.sh
+++ bin/goup.sh
@@ -3,14 +3,14 @@
 . $(dirname "$0")/common.sh
 
 if ask "Install common go packages?"; then
-  rm -rf $GOPATH/bin/*
-  rm -rf $GOPATH/pkg/*
+  # rm -rf $GOPATH/bin/*
+  # rm -rf $GOPATH/pkg/*
 
-  go get -u -v github.com/nsf/gocode
-  check $?
-
-  # go get -u github.com/mdempsky/gocode
+  # go get -u -v github.com/nsf/gocode
   # check $?
+
+  go get -u -v github.com/mdempsky/gocode
+  check $?
 
   go get -u -v github.com/rogpeppe/godef
   check $?
