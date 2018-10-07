2992eb7d0ffc555bd3ddf936b15d66082ed2bcc5
Julio Cesar Villasante <jvillasantegomez@gmail.com>
2018-10-07 09:23:36 -0400

    More changes

--- bin/cpp_project_template/.dir-locals.el
+++ bin/cpp_project_template/.dir-locals.el
@@ -1,5 +1,4 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 ((c++-mode
-  (flycheck-clang-language-standard . "c++17")
-  (eval add-hook 'before-save-hook #'clang-format-buffer nil t)))
+  (flycheck-clang-language-standard . "c++17")))
