f72695996be17e6bc0e46c09f213a3bc53e3afc5
Julio Cesar Villasante <jvillasantegomez@gmail.com>
2018-12-10 19:20:54 -0500

    Changes to common-lisp layer

--- Home/.spacemacs.d/layers/jv-lisp/config.el
+++ Home/.spacemacs.d/layers/jv-lisp/config.el
@@ -1,6 +1,13 @@
+(setq inferior-lisp-program "/usr/local/bin/sbcl")
+
 (setq slime-lisp-implementations
-  '((cmucl ("cmucl" "-quiet"))
-     (sbcl ("/usr/local/bin/sbcl" "+R" "-l" "~/.sbclrc" "run") :coding-system utf-8-unix)))
+  `((ccl ("~/.cim/bin/ccl-1.9") :coding-system utf-8-unix)
+     (alisp ("/usr/local/bin/alisp") :coding-system utf-8-unix)
+     (ecl ("/usr/local/bin/ecl"))  ; :coding-system utf-8-unix)
+     (cmucl ("/usr/local/bin/cmucl") :coding-system utf-8-unix)
+     (sbcl ("/usr/local/bin/sbcl" "+R" "-l" "~/.sbclrc") :coding-system utf-8-unix)
+     (abcl ("~/.cim/bin/abcl-1.3.1" "-XX:MaxPermSize=256m" "-Dfile.encoding=UTF-8") :coding-system utf-8-unix)
+     (clisp ("/usr/local/bin/clisp") :coding-system utf-8-unix)))
 
 (setq slime-default-lisp 'sbcl)
 (setq slime-net-coding-system 'utf-8-unix)
