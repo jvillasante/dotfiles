2a0b5ed99277d3a238c8a5f9c7ccdd3c0a1b2352
Julio Cesar Villasante <jvillasantegomez@gmail.com>
2019-02-09 07:57:55 -0500

    Changes to lsp layer

--- Home/.spacemacs.d/layers/jv-lsp/packages.el
+++ Home/.spacemacs.d/layers/jv-lsp/packages.el
@@ -3,18 +3,21 @@
      lsp-ui))
 
 (defun jv-lsp/post-init-lsp-mode ()
-  (spacemacs|diminish lsp-mode " Ⓛ" " L")
+  (defun jv-lsp/setup-lsp-mode ()
+    (setq lsp-remap-xref-keybindings nil)
+    (setq lsp-navigation 'both))
 
-  (add-hook 'c++-mode-hook
-    (lambda () (progn
-                 (setq lsp-remap-xref-keybindings nil)
-                 (setq lsp-navigation 'both)))))
+  (spacemacs|diminish lsp-mode " Ⓛ" " L")
+  (add-hook 'c-mode-hook 'jv-lsp/setup-lsp-mode)
+  (add-hook 'c++-mode-hook 'jv-lsp/setup-lsp-mode))
 
 (defun jv-lsp/post-init-lsp-ui ()
-  (add-hook 'c++-mode-hook
-    (lambda () (progn
-                 (setq lsp-ui-doc-enable t)
-                 (setq lsp-ui-doc-include-signature nil)
-                 (setq lsp-ui-sideline-enable nil)
-                 (setq lsp-ui-sideline-show-symbol nil)
-                 (setq lsp-ui-sideline-ignore-dupliate nil)))))
+  (defun jv-lsp/setup-lsp-ui-mode ()
+    (setq lsp-ui-doc-enable t)
+    (setq lsp-ui-doc-include-signature nil)
+    (setq lsp-ui-sideline-enable nil)
+    (setq lsp-ui-sideline-show-symbol nil)
+    (setq lsp-ui-sideline-ignore-dupliate nil))
+
+  (add-hook 'c-mode-hook 'jv-lsp/setup-lsp-ui-mode)
+  (add-hook 'c++-mode-hook 'jv-lsp/setup-lsp-ui-mode))
