37d0c54acbf7a8928e1730a77f1308b3342756d2
Julio C. Villasante <jvillasantegomez@gmail.com>
2019-03-17 10:07:35 -0400

    Back to cache directory for ccls

--- Home/.spacemacs.d/init.el
+++ Home/.spacemacs.d/init.el
@@ -92,6 +92,7 @@ This function should only modify configuration layer settings."
                  c-c++-default-mode-for-headers 'c++-mode
                  c-c++-backend 'lsp-ccls
                  c-c++-lsp-executable (file-truename "~/Hacking/software/ccls/Release/ccls")
+                 c-c++-lsp-cache-dir (file-truename "~/Hacking/workspace/dotfiles/.emacs.d/.cache/lsp-ccls")
                  c-c++-adopt-subprojects t
                  ;; c-c++-enable-clang-support t ;; not if lsp is enabled!
                  c-c++-enable-clang-format-on-save t)
