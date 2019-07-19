b2d675b9507170579724cbb87770b75442aa316b
Julio C. Villasante <Julio.Villasante.consultant@nielsen.com>
2019-07-19 13:33:34 -0400

    Small addition to brewup.sh file

--- Home/bin/brewup.sh
+++ Home/bin/brewup.sh
@@ -7,6 +7,7 @@ if hash brew 2>/dev/null; then
   if ask "Do you want to update brew?"; then
     brew update
     brew upgrade
+    brew cask upgrade
     brew cask outdated | cut -f 1 | xargs brew cask reinstall
   else
     echo "Not updating brew."
