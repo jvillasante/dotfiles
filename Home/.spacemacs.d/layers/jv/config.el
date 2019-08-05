fa188b765e3861b920405fea26d7a663c4128159
Julio C. Villasante <jvillasantegomez@gmail.com>
2019-08-05 18:51:42 -0400

    Adds comment box to jv/config.el

--- Home/.spacemacs.d/layers/jv/config.el
+++ Home/.spacemacs.d/layers/jv/config.el
@@ -5,7 +5,6 @@
 (defun jv/comment-box (b e)
     "Draw a box comment around the region but arrange for the region to extend to
 at least the fill column. Place the point after the comment box."
-
     (interactive "r")
 
     (let ((e (copy-marker e t)))
@@ -17,8 +16,10 @@ at least the fill column. Place the point after the comment box."
         (set-marker e nil)))
 (global-set-key (kbd "C-c b b") 'jv/comment-box)
 
-;; Keeps the compilation buffer if there are warnings or errors,
-;; and buries it otherwise (after 1 second).
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Keeps the compilation buffer if there are warnings or errors,                                                  ;;
+;; and buries it otherwise (after 1 second).                                                                      ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun jv/bury-compile-buffer-if-successful (buffer string)
     "Bury a compilation buffer if succeeded without warnings "
     (when (and
@@ -37,7 +38,9 @@ at least the fill column. Place the point after the comment box."
             buffer)))
 (add-hook 'compilation-finish-functions 'jv/bury-compile-buffer-if-successful)
 
-;; ANSI-colors in the compilation buffer output
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ANSI-colors in the compilation buffer output                                                                   ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (require 'ansi-color)
 (defun jv/colorize-compilation ()
     "Colorize from `compilation-filter-start' to `point'."
@@ -46,7 +49,10 @@ at least the fill column. Place the point after the comment box."
             compilation-filter-start (point))))
 (add-hook 'compilation-filter-hook #'jv/colorize-compilation)
 
-;; http://emacsredux.com/blog/2013/04/28/switch-to-previous-buffer/
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Switch to previously open buffer
+;; http://emacsredux.com/blog/2013/04/28/switch-to-previous-buffer/                                               ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun jv/switch-to-previous-buffer ()
     "Switch to previously open buffer.
 Repeated invocations toggle between the two most recently open buffers."
@@ -55,29 +61,30 @@ Repeated invocations toggle between the two most recently open buffers."
 (evil-leader/set-key
     "ob" 'jv/switch-to-previous-buffer)
 
-;;; image-dimensions-minor-mode.el
-;;
-;; Display the image dimensions in the mode line, when viewing an image.
-;;
-;; Author: Phil S.
-;;
-;; Compatibility: GNU Emacs 24.3
-;;
-;; Installation:
-;; (eval-after-load 'image-mode '(require 'image-dimensions-minor-mode))
-
-;; Commentary:
-;;
-;; To also see this information in the frame title, your `frame-title-format'
-;; variable might be something like the following:
-;;
-;; (setq frame-title-format
-;;       '(buffer-file-name
-;;         ("%b (Emacs) %f" image-dimensions-minor-mode-dimensions)
-;;         (dired-directory
-;;          (:eval (concat (buffer-name) " (Emacs) " dired-directory))
-;;          ("%b (Emacs)"))))
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; image-dimensions-minor-mode.el                                                                                 ;;
+;;                                                                                                                ;;
+;; Display the image dimensions in the mode line, when viewing an image.                                          ;;
+;;                                                                                                                ;;
+;; Author: Phil S.                                                                                                ;;
+;;                                                                                                                ;;
+;; Compatibility: GNU Emacs 24.3                                                                                  ;;
+;;                                                                                                                ;;
+;; Installation:                                                                                                  ;;
+;; (eval-after-load 'image-mode '(require 'image-dimensions-minor-mode))                                          ;;
+;;                                                                                                                ;;
+;; Commentary:                                                                                                    ;;
+;;                                                                                                                ;;
+;; To also see this information in the frame title, your `frame-title-format'                                     ;;
+;; variable might be something like the following:                                                                ;;
+;;                                                                                                                ;;
+;; (setq frame-title-format                                                                                       ;;
+;;       '(buffer-file-name                                                                                       ;;
+;;         ("%b (Emacs) %f" image-dimensions-minor-mode-dimensions)                                               ;;
+;;         (dired-directory                                                                                       ;;
+;;          (:eval (concat (buffer-name) " (Emacs) " dired-directory))                                            ;;
+;;          ("%b (Emacs)"))))                                                                                     ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (with-eval-after-load 'image-mode
     (eval-when-compile (require 'cl-macs))
 
