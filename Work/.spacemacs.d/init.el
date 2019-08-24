b1ee6e8fe80fc6b65478a0a0d5f9366981c8686d
Julio C. Villasante <jvillasantegomez@gmail.com>
2019-08-24 11:45:31 -0400

    No agenda and todos on startup screen

--- Work/.spacemacs.d/init.el
+++ Work/.spacemacs.d/init.el
@@ -249,8 +249,8 @@ It should only modify the values of Spacemacs settings."
         ;; `spacemacs-buffer-startup-lists-length' takes effect.
         dotspacemacs-startup-lists '((recents . 5)
                                         (projects . 10)
-                                        (agenda . 5)
-                                        (todos . 5)
+                                        ;; (agenda . 5)
+                                        ;; (todos . 5)
                                         bookmarks)
 
         ;; True if the home buffer should respond to resize events. (default t)
