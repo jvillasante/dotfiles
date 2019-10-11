f398befabd32bdbcc5cc848fbfb14dbeb6d0da6e
Julio C. Villasante <julio.c.villasante@nielsen.com>
2019-10-11 12:50:33 -0400

    Adds `SPC g g` for magit-status

--- Home/.spacemacs.d/layers/jv-magit/config.el
+++
@@ -1,5 +0,0 @@
-(with-eval-after-load 'magit
-    (setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1)
-    (setq-default git-magit-status-fullscreen t)
-    (setq magit-completing-read-function 'magit-builtin-completing-read
-        magit-push-always-verify nil))
