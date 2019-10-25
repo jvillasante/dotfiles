918d750ee1fda5eb7267e2e2e7252091a555a854
Julio C. Villasante <jvillasantegomez@gmail.com>
2019-10-25 12:07:46 -0400

    Adds mu4e config but decided not to use it anywyas.
    Keeping the config for future reference.

---
+++ Home/.offlineimap.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+# import json
+# import subprocess
+
+# def secure_string_for(account, service, value):
+#     # this relies on the macOS `security` tool
+#     # https://developer.apple.com/legacy/library/documentation/Darwin/Reference/ManPages/man1/security.1.html
+#     return json.loads(subprocess.check_output(["security",
+#                                                "find-generic-password",
+#                                                "-a", account,
+#                                                "-s", service,
+#                                                "-w"]).strip())[value]
+
+import re, subprocess
+def get_keychain_pass(account=None, server=None):
+    params = {
+        'security': '/usr/bin/security',
+        'command': 'find-internet-password',
+        'account': account,
+        'server': server,
+        'keychain': '/Users/`whoami`/Library/Keychains/login.keychain',
+    }
+    command = "sudo -u `whoami` %(security)s -v %(command)s -g -a %(account)s -s %(server)s %(keychain)s" % params
+    output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
+    outtext = [l for l in output.splitlines()
+               if l.startswith('password: ')][0]
+
+    return re.match(r'password: "(.*)"', outtext).group(1)
