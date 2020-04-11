9728ddcd4e31fbe59f568365eb8bed77f9c03189
Julio C. Villasante <jvillasantegomez@gmail.com>
2020-04-11 10:57:44 -0400

    do not byte compile emacs pacakges, not worhty

--- Common/bin/update_emacs.sh
+++ Common/bin/update_emacs.sh
@@ -10,20 +10,11 @@ if ask "Do you want to update your dotfiles"; then
 fi
 
 if ask "Do you want to upgrade emacs?"; then
-    ~/.emacs.d/bin/doom clean
-    check $?
-
     ~/.emacs.d/bin/doom -y upgrade -f
-    check $?
-
-    ~/.emacs.d/bin/doom compile :core
     check $?
 fi
 
 if ask "Do you want to sync emacs?"; then
-    # ~/.emacs.d/bin/doom update
-    # check $?
-
     ~/.emacs.d/bin/doom sync
     check $?
 fi
