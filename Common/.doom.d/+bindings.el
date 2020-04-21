f858fa08b1b31a69a65b23869bbe1198c793d688
Julio C. Villasante <jvillasantegomez@gmail.com>
2020-04-20 20:29:56 -0400

    Neotree stretch on TAB

--- Common/.doom.d/+bindings.el
+++ Common/.doom.d/+bindings.el
@@ -195,6 +195,7 @@
 
     (:after neotree
         (:map neotree-mode-map
+            :desc "Neotree Stretch" :n [tab] #'neotree-stretch-toggle
             :desc "Neotree Hydra" :n "?" #'+my/hydra-neotree/body))
 
     (:after elfeed
