c2c694722136a043c2ddc35a1b283a98ba62878d
Julio C. Villasante <jvillasantegomez@gmail.com>
2020-04-30 22:38:12 -0400

    Removes spacemacs (I'm totally a doom person now)

--- Work/.spacemacs.d/layers/jv-cpp-common/funcs.el
+++
@@ -1,4 +0,0 @@
-(defun jv-cpp/rtags-find-symbol-at-point-other-file ()
-  (interactive)
-  (let((current-prefix-arg '(4)))
-    (call-interactively 'rtags-find-symbol-at-point)))
--- Work/.spacemacs.d/layers/jv-themes/config.el
+++
@@ -1,6 +0,0 @@
-;;; set global key for hydra
-(global-set-key (kbd "C-c w t") 'jv/themes-hydra/body)
-
-;;; load default theme
-(add-hook 'after-init-hook
-    (lambda () (load-theme 'doom-solarized-light t)))
