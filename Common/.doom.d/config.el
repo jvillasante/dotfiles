e2c10c69db3a2dd687d4943d2dae7ec5b8d7ed06
Julio C. Villasante <julio.c.villasante@nielsen.com>
2020-05-14 17:19:38 -0400

    change change

--- Common/.doom.d/config.el
+++ Common/.doom.d/config.el
@@ -1,6 +1,5 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; change
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
 
