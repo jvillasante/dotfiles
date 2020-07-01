e4e3790a3421b05fa5ce1dd5fb471c6d94e50f7b
Julio C. Villasante <jvillasantegomez@gmail.com>
2020-07-01 13:09:15 -0400

    Some changes to cpp projects

--- Common/.bin/cpp_project_template_make/.dir-locals.el
+++ Common/.bin/cpp_project_template_make/.dir-locals.el
@@ -1,4 +1,15 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
-((c++-mode
-  (flycheck-clang-language-standard . "c++17")))
+((c++-mode .
+  ((flycheck-clang-language-standard . "c++20"))))
+
+((c++-mode .
+     ((lsp-clients-clangd-args . ("-j=2"
+                                     "--log=error"
+                                     ;; "--compile-commands-dir=build"
+                                     "--background-index"
+                                     "--clang-tidy"
+                                     "--completion-style=detailed"
+                                     "--pch-storage=memory"
+                                     "--header-insertion=never"
+                                     "--header-insertion-decorators=0")))))
