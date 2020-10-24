064e94470545f9bcd53ef585148a5684da47debc
Julio C. Villasante <jvillasantegomez@gmail.com>
2020-10-24 08:03:39 -0400

    Adds treemacs binding and C++20 to cpp template

--- Common/.doom.d/+bindings.el
+++ Common/.doom.d/+bindings.el
@@ -192,6 +192,10 @@
             :desc "Neotree Stretch" :n [tab] #'neotree-stretch-toggle
             :desc "Neotree Hydra" :n "?" #'+my/hydra-neotree/body))
 
+    (:after treemacs
+        (:map treemacs-mode-map
+            :desc "Current Project" :n [tab] #'treemacs-display-current-project-exclusively))
+
     (:after elfeed
         (:map elfeed-search-mode-map
             :desc "Elfeed update"         :n "gr" #'elfeed-update
