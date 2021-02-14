e611ab42625b6cac711d0638a26e34a16f50842d
Julio C. Villasante <jvillasantegomez@gmail.com>
2021-02-14 09:51:34 -0500

    changes to utils.hpp on cpp project template

--- Common/.bin/cpp_project_template_cmake/include/utils.hpp
+++ Common/.bin/cpp_project_template_cmake/include/utils.hpp
@@ -36,6 +36,25 @@ constexpr void unused(Ts&&... ts) {
 }
 
 namespace functional {
+namespace mutable_version {
+template <typename F, typename R>
+void mapf(F&& f, R& r) {
+    std::transform(std::begin(r), std::end(r), std::begin(r), std::forward<F>(f));
+}
+} // namespace mutable_version
+
+namespace variadic_function_template_version {
+template <typename F, typename T1, typename T2>
+auto foldl(F&& f, T1 arg1, T2 arg2) {
+    return f(arg1, arg2);
+}
+
+template <typename F, typename T, typename... Ts>
+auto foldl(F&& f, T&& head, Ts... rest) {
+    return f(head, foldl(std::forward<F>(f), rest...));
+}
+} // namespace variadic_function_template_version
+
 template <typename F, typename R>
 R mapf(F&& f, R r) {
     std::transform(std::begin(r), std::end(r), std::begin(r), std::forward<F>(f));
@@ -43,21 +62,26 @@ R mapf(F&& f, R r) {
 }
 
 template <typename F, typename T, typename U>
-std::map<T, U> mapf(F&& f, std::map<T, U> const& m) {
-    std::map<T, U> r{};
-    for (auto const& kvp : m) {
-        r.insert(f(kvp));
+std::map<T, U> mapf(F&& f, const std::map<T, U>& m) {
+    std::map<T, U> r;
+    for (auto&& kvp : m) {
+        // r.insert(f(kvp));
+        r.insert(f(std::forward<decltype(kvp)>(kvp)));
     }
+
     return r;
 }
 
 template <typename F, typename T>
 std::queue<T> mapf(F&& f, std::queue<T> q) {
-    std::queue<T> r{};
+    std::queue<T> r;
+
     while (!q.empty()) {
-        r.push(f(q.front()));
+        // r.push(f(q.front()));
+        r.push(f(std::forward<T>(q.front())));
         q.pop();
     }
+
     return r;
 }
 
@@ -68,13 +92,18 @@ constexpr T foldl(F&& f, R&& r, T i) {
 
 template <typename F, typename R, typename T>
 constexpr T foldr(F&& f, R&& r, T i) {
-    return std::accumulate(std::rbegin(r), std::rend(r), std::move(i), std::forward<F>(f));
+    // return std::accumulate(std::rbegin(r), std::rend(r), std::move(i), std::forward<F>(f));
+    return std::accumulate(
+        std::rbegin(r), std::rend(r), std::move(i), [f](auto&& arg1, auto&& arg2) {
+            return f(std::forward<decltype(arg2)>(arg2), std::forward<decltype(arg1)>(arg1));
+        });
 }
 
 template <typename F, typename T>
 constexpr T foldl(F&& f, std::queue<T> q, T i) {
     while (!q.empty()) {
-        i = f(i, q.front());
+        // i = f(i, q.front());
+        i = f(std::forward<T>(i), std::forward<T>(q.front()));
         q.pop();
     }
 
