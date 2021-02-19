e4543640bcde31d6ea875d6f14802b02c673e106
Julio C. Villasante <jvillasantegomez@gmail.com>
2021-02-19 13:07:38 -0500

    Changes to scripts indentation and go install script

--- Common/.bin/common.sh
+++ Common/.bin/common.sh
@@ -73,47 +73,47 @@ find_dotfiles() {
 # Check helper for return values
 #
 check() {
-  if [ $1 -ne 0 ]; then
-    echo ""
-    echo ">>> This is an error, do something else... We don't know what's wrong here!!!"
-    echo ""
-    exit $1
-  fi
+    if [ $1 -ne 0 ]; then
+        echo ""
+        echo ">>> This is an error, do something else... We don't know what's wrong here!!!"
+        echo ""
+        exit $1
+    fi
 }
 
 #
 # Ask helper (https://djm.me/ask)
 # 
 ask() {
-  local prompt default reply
+    local prompt default reply
 
-  while true; do
-    if [ "${2:-}" = "Y" ]; then
-      prompt="Y/n"
-      default=Y
-    elif [ "${2:-}" = "N" ]; then
-      prompt="y/N"
-      default=N
-    else
-      prompt="y/n"
-      default=
-    fi
+    while true; do
+        if [ "${2:-}" = "Y" ]; then
+            prompt="Y/n"
+            default=Y
+        elif [ "${2:-}" = "N" ]; then
+            prompt="y/N"
+            default=N
+        else
+            prompt="y/n"
+            default=
+        fi
 
-    # Ask the question (not using "read -p" as it uses stderr not stdout)
-    echo "$1 [$prompt] "
+        # Ask the question (not using "read -p" as it uses stderr not stdout)
+        echo "$1 [$prompt] "
 
-    # Read the answer (use /dev/tty in case stdin is redirected from somewhere else)
-    read reply </dev/tty
+        # Read the answer (use /dev/tty in case stdin is redirected from somewhere else)
+        read reply </dev/tty
 
-    # Default?
-    if [ -z "$reply" ]; then
-      reply=$default
-    fi
+        # Default?
+        if [ -z "$reply" ]; then
+            reply=$default
+        fi
 
-    # Check if the reply is valid
-    case "$reply" in
-      Y*|y*) return 0 ;;
-      N*|n*) return 1 ;;
-    esac
-  done
+        # Check if the reply is valid
+        case "$reply" in
+            Y*|y*) return 0 ;;
+            N*|n*) return 1 ;;
+        esac
+    done
 }
--- Common/.bin/mac_update.sh
+++ Common/.bin/mac_update.sh
@@ -8,25 +8,25 @@ echo ""
 
 # Check if brew is installed and perform maintainance
 if hash brew 2>/dev/null; then
-  if ask "Do you want to update brew?"; then
-    brew update
-    brew upgrade
-    brew upgrade --cask --greedy
-    mas upgrade
-  else
-    echo "Not updating brew."
-  fi
+    if ask "Do you want to update brew?"; then
+        brew update
+        brew upgrade
+        brew upgrade --cask --greedy
+        mas upgrade
+    else
+        echo "Not updating brew."
+    fi
 
-  if ask "Do you want to run brew doctor?"; then
-    brew doctor
-    brew missing
-  else
-    echo "Not runing brew doctor."
-  fi
+    if ask "Do you want to run brew doctor?"; then
+        brew doctor
+        brew missing
+    else
+        echo "Not runing brew doctor."
+    fi
 
-  if ask "Do you want to cleanup brew?"; then
-    brew cleanup -s
-  else
-    echo "Not cleaning up brew."
-  fi
+    if ask "Do you want to cleanup brew?"; then
+        brew cleanup -s
+    else
+        echo "Not cleaning up brew."
+    fi
 fi
