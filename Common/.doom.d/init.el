31139cd6c1b0dcf69e562f073728b983d3edab06
Julio C. Villasante <jvillasantegomez@gmail.com>
2021-03-25 14:33:46 -0400

    Small change

--- Common/.doom.d/init.el
+++ Common/.doom.d/init.el
@@ -112,7 +112,8 @@
         +peek)
     magit             ; a git porcelain for Emacs
     make              ; run make tasks from Emacs
-    (pass +auth)      ; password manager for nerds
+    (pass             ; password manager for nerds
+        +auth)
     ;;pdf               ; pdf enhancements
     ;;prodigy           ; FIXME managing external services & code builders
     ;;rgb               ; creating color strings
