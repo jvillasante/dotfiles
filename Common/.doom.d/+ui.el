2c59abbf7cfd1bad0b1d5d1409aef606461e61c4
Julio C. Villasante <jvillasantegomez@gmail.com>
2021-04-06 12:53:36 -0400

    Adds popup rule for *cargo-test*

--- Common/.doom.d/+ui.el
+++ Common/.doom.d/+ui.el
@@ -50,3 +50,4 @@
 (set-popup-rule! "^\\*osx-dictionary" :size 0.4 :quit t :ttl 0)
 (set-popup-rule! "^\\*Password-Store" :side 'left :size 0.4 :quit nil)
 (set-popup-rule! "^\\*ivy-occur" :size 0.4 :ttl 0 :quit nil)
+(set-popup-rule! "^\\*cargo-test*" :size 0.4 :ttl 0 :quit nil)
