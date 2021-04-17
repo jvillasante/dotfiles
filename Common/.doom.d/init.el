9432ecf2b526e75cafa6a6d2521b604fef8c5460
Julio C. Villasante <jvillasantegomez@gmail.com>
2021-04-17 09:31:36 -0400

    Adds zig (commented out) to langs

--- Common/.doom.d/init.el
+++ Common/.doom.d/init.el
@@ -188,6 +188,7 @@
     ;;terra             ; Earth and Moon in alignment for performance.
     web               ; the tubes
     yaml              ; JSON, but readable
+    ;;zig               ; C, but simpler
 
     :email
     ;;(mu4e +gmail)
