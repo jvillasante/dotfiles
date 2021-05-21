a9a2b8dc88b2ec750ab261342d6167729901868f
Julio C. Villasante <jvillasantegomez@gmail.com>
2021-05-21 08:02:33 -0400

    Adds JS prettier and other changes

--- Common/.doom.d/config.el
+++ Common/.doom.d/config.el
@@ -166,7 +166,6 @@
     ;; Disable format on web stuff
     (add-to-list '+format-on-save-enabled-modes 'text-mode 'append)
     (add-to-list '+format-on-save-enabled-modes 'web-mode 'append)
-    (add-to-list '+format-on-save-enabled-modes 'js2-mode 'append)
     (add-to-list '+format-on-save-enabled-modes 'gitignore-mode 'append)
 
     ;; Do now format with lsp, use `format` instead
