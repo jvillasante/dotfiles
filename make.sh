b97b8653b72afc54c003de024f0e8622a1f51c4c
Julio C. Villasante <jvillasantegomez@gmail.com>
2021-05-22 09:41:19 -0400

    Nix and Git
    * Starting up with nix
    * Adds git config to ~/.config/git

--- make.sh
+++ make.sh
@@ -14,69 +14,69 @@ DOTFILES_DIR=$(find_dotfiles)
 echo "===================================================================================="
 echo ">> Running for '$CURRENT_ENV' on '$CURRENT_OS' at '$DOTFILES_DIR'."
 
-install_zsh () {
-    # Test to see if zshell is installed.  If it is:
-    if [ $CURRENT_OS = "LINUX" ]; then
-        if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
-            # Clone my oh-my-zsh repository from GitHub only if it isn't already present
-            if [ ! -d $DOTFILES_DIR/.oh-my-zsh/ ]; then
-                git clone http://github.com/robbyrussell/oh-my-zsh.git $DOTFILES_DIR/.oh-my-zsh
-            fi
+install_zsh() {
+	# Test to see if zshell is installed.  If it is:
+	if [ $CURRENT_OS = "LINUX" ]; then
+		if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
+			# Clone my oh-my-zsh repository from GitHub only if it isn't already present
+			if [ ! -d $DOTFILES_DIR/.oh-my-zsh/ ]; then
+				git clone http://github.com/robbyrussell/oh-my-zsh.git $DOTFILES_DIR/.oh-my-zsh
+			fi
 
-            # Set the default shell to zsh if it isn't currently set to zsh
-            if [ ! $(echo $SHELL) = $(which zsh) ]; then
-                chsh -s $(which zsh)
-            fi
-        else
-            # If zsh isn't installed, If the platform is Linux, try an apt-get to install zsh and then recurse
-            sudo apt-get install zsh
-            install_zsh
-        fi
-    elif [ $CURRENT_OS = "OSX" ]; then
-        if [ -f /usr/local/bin/zsh -o -f /usr/bin/zsh ]; then
-            # Clone my oh-my-zsh repository from GitHub only if it isn't already present
-            if [ ! -d $DOTFILES_DIR/.oh-my-zsh/ ]; then
-                git clone http://github.com/robbyrussell/oh-my-zsh.git $DOTFILES_DIR/.oh-my-zsh
-            fi
+			# Set the default shell to zsh if it isn't currently set to zsh
+			if [ ! $(echo $SHELL) = $(which zsh) ]; then
+				chsh -s $(which zsh)
+			fi
+		else
+			# If zsh isn't installed, If the platform is Linux, try an apt-get to install zsh and then recurse
+			sudo apt-get install zsh
+			install_zsh
+		fi
+	elif [ $CURRENT_OS = "OSX" ]; then
+		if [ -f /usr/local/bin/zsh -o -f /usr/bin/zsh ]; then
+			# Clone my oh-my-zsh repository from GitHub only if it isn't already present
+			if [ ! -d $DOTFILES_DIR/.oh-my-zsh/ ]; then
+				git clone http://github.com/robbyrussell/oh-my-zsh.git $DOTFILES_DIR/.oh-my-zsh
+			fi
 
-            echo ">> Remember to set the default shell to zsh if it isn't already         "
-        else
-            echo ">> Remember to install zsh                                              "
-        fi
-    fi
+			echo ">> Remember to set the default shell to zsh if it isn't already         "
+		else
+			echo ">> Remember to install zsh                                              "
+		fi
+	fi
 }
 
 # CURRENT_VIM_DISTRO="vim"
 CURRENT_VIM_DISTRO="nvim"
 install_vimplug() {
-    if [ $CURRENT_VIM_DISTRO = "nvim" ]; then
-        if [ ! -f ~/.local/share/nvim/site/autoload/plug.vim ]; then
-            curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-                https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-        fi
-    elif [ $CURRENT_VIM_DISTRO = "vim" ]; then
-        if [ ! -f ~/.vim/autoload/plug.vim ]; then
-            curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-                https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-        fi
-    else
-        echo "Set CURRENT_VIM_DISTRO!"
-    fi
+	if [ $CURRENT_VIM_DISTRO = "nvim" ]; then
+		if [ ! -f ~/.local/share/nvim/site/autoload/plug.vim ]; then
+			curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+				https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+		fi
+	elif [ $CURRENT_VIM_DISTRO = "vim" ]; then
+		if [ ! -f ~/.vim/autoload/plug.vim ]; then
+			curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+				https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+		fi
+	else
+		echo "Set CURRENT_VIM_DISTRO!"
+	fi
 }
 
 # CURRENT_EMACS_DISTRO="SPACEMACS"
 CURRENT_EMACS_DISTRO="DOOM_EMACS"
-install_emacs () {
-    if [ ! -d $DOTFILES_DIR/.emacs.d/ ]; then
-        if [ $CURRENT_EMACS_DISTRO = "SPACEMACS" ]; then
-            git clone -b develop --single-branch git@github.com:syl20bnr/spacemacs.git $DOTFILES_DIR/.emacs.d
-        elif [ $CURRENT_EMACS_DISTRO = "DOOM_EMACS" ]; then
-            git clone -b develop --single-branch git@github.com:hlissner/doom-emacs.git $DOTFILES_DIR/.emacs.d
-            $DOTFILES_DIR/.emacs.d/bin/doom install
-        else
-            echo "Set CURRENT_EMACS_DISTRO!"
-        fi
-    fi
+install_emacs() {
+	if [ ! -d $DOTFILES_DIR/.emacs.d/ ]; then
+		if [ $CURRENT_EMACS_DISTRO = "SPACEMACS" ]; then
+			git clone -b develop --single-branch git@github.com:syl20bnr/spacemacs.git $DOTFILES_DIR/.emacs.d
+		elif [ $CURRENT_EMACS_DISTRO = "DOOM_EMACS" ]; then
+			git clone -b develop --single-branch git@github.com:hlissner/doom-emacs.git $DOTFILES_DIR/.emacs.d
+			$DOTFILES_DIR/.emacs.d/bin/doom install
+		else
+			echo "Set CURRENT_EMACS_DISTRO!"
+		fi
+	fi
 }
 
 install_zsh
@@ -86,39 +86,43 @@ install_emacs
 echo ">> Linking global files in ~/home..."
 files=".emacs.d .oh-my-zsh"
 for file in $files; do
-    unlink ~/$file
-    ln -s $DOTFILES_DIR/$file ~/
+	unlink ~/$file
+	ln -s $DOTFILES_DIR/$file ~/
 done
 
 echo ">> Linking common files in ~/home..."
 files=".doom.d .oh-my-zsh.d .bin .profile .bashrc .zshenv .zshrc .tmux.conf .tmux-macos.conf .tmux-linux.conf .clang-tidy .editorconfig .jsbeautifyrc .jshintrc .offlineimaprc .offlineimap.py .mbsyncrc .msmtprc .sbclrc .rustfmt.toml .ycm_extra_conf.py"
 for file in $files; do
-    unlink ~/$file
-    ln -s $DOTFILES_DIR/Common/$file ~/
+	unlink ~/$file
+	ln -s $DOTFILES_DIR/Common/$file ~/
 done
 
 echo ">> Linking other files in ~/home..."
-files=".ccls .clang-format .fzf.zsh .gitconfig"
+files=".ccls .clang-format .fzf.zsh"
 for file in $files; do
-    unlink ~/$file
+	unlink ~/$file
 
-    if [ $CURRENT_ENV = "HOME" ]; then
-        ln -s $DOTFILES_DIR/Home/$file ~/
-    elif [ $CURRENT_ENV = "WORK" ]; then
-        ln -s $DOTFILES_DIR/Work/$file ~/
-    fi
+	if [ $CURRENT_ENV = "HOME" ]; then
+		ln -s $DOTFILES_DIR/Home/$file ~/
+	elif [ $CURRENT_ENV = "WORK" ]; then
+		ln -s $DOTFILES_DIR/Work/$file ~/
+	fi
 done
 
-echo ">> Linking other files in ~/.config"
-files="alacritty"
-for file in $files; do
-    unlink ~/.config/$file
+echo ">> Linking nix files in ~/.config..."
+unlink ~/.config/nixpkgs
+ln -s $DOTFILES_DIR/Nix ~/.config/nixpkgs
 
-    if [ $CURRENT_ENV = "HOME" ]; then
-        ln -s $DOTFILES_DIR/Home/$file ~/.config
-    elif [ $CURRENT_ENV = "WORK" ]; then
-        ln -s $DOTFILES_DIR/Work/$file ~/.config
-    fi
+echo ">> Linking other files in ~/.config"
+files="alacritty git"
+for file in $files; do
+	unlink ~/.config/$file
+
+	if [ $CURRENT_ENV = "HOME" ]; then
+		ln -s $DOTFILES_DIR/Home/$file ~/.config
+	elif [ $CURRENT_ENV = "WORK" ]; then
+		ln -s $DOTFILES_DIR/Work/$file ~/.config
+	fi
 done
 
 echo ">> Done!"
