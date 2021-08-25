87e35839e28fbb494dce8b8c4ddff2bfe74e655b
Julio C. Villasante <julio.c.villasante@nielsen.com>
2021-08-25 13:56:20 -0400

    Changes to rofi

--- Common/rofi/bin/app_launcher.sh
+++ Common/rofi/bin/app_launcher.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-rofi -combi-modi window,drun,ssh -show combi -modi combi -dpi 1
+rofi -combi-modi window,drun,run,ssh -show combi -modi combi \
+    -normal-window -width 100 -location 1 -lines 25 -bw 2 -yoffset -2
