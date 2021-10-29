3edff8ee64a8678e8152ec400a57daf4fbcfbaab
Julio C. Villasante <jvillasantegomez@gmail.com>
2021-10-29 14:05:23 -0400

    Adds brew reinstall all pacakges to mac.sh
    Useful after an OS update

--- Common/.bin/mac.sh
+++ Common/.bin/mac.sh
@@ -41,9 +41,16 @@ brew_all() {
     brew_doctor
 }
 
+brew_reinstall_all() {
+    if hash brew 2>/dev/null; then
+        echo "Reinstalling all brew packages..."
+        brew list --full-name | xargs brew reinstall -v
+    fi
+}
+
 while true; do
     PS3="Choose an option: "
-    options=("Brew update" "Brew Doctor" "Brew cleanup" "Brew all" "Quit")
+    options=("Brew Update" "Brew Cleanup" "Brew Doctor" "Brew All" "Brew Reinstall All Packages" "Quit")
 
     select opt in "${options[@]}"; do
         case $REPLY in
@@ -63,7 +70,11 @@ while true; do
                 brew_all
                 break
                 ;;
-            5) break 2 ;;
+            5)
+                brew_reinstall_all
+                break
+                ;;
+            6) break 2 ;;
             *) echo "Invalid option '$opt'" >&2 ;;
         esac
     done
