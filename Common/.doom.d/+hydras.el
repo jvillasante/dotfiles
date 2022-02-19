7ced37553e9d74a6dca42eadc17a50480125f856
Julio C. Villasante <jvillasantegomez@gmail.com>
2022-02-19 06:35:28 -0500

    Adds multiple cursors hydra

--- Common/.doom.d/+hydras.el
+++ Common/.doom.d/+hydras.el
@@ -105,4 +105,33 @@ Navigation^^^^             Actions^^         Visual actions/config^^^
             ("R" neotree-change-root)
             ("f" neotree-find)
             ("s" neotree-hidden-file-toggle)))
+
+    (after! evil-mc
+        (defhydra +my/hydra-evil-mc (:color pink
+                                        :hint nil
+                                        :pre (evil-mc-pause-cursors))
+            "
+^Match^            ^Line-wise^           ^Manual^
+^^^^^^----------------------------------------------------
+_Z_: match all     _J_: make & go down   _z_: toggle here
+_m_: make & next   _K_: make & go up     _r_: remove last
+_M_: make & prev   ^ ^                   _R_: remove all
+_n_: skip & next   ^ ^                   _p_: pause/resume
+_N_: skip & prev
+
+Current pattern: %`evil-mc-pattern
+"
+            ("Z" #'evil-mc-make-all-cursors)
+            ("m" #'evil-mc-make-and-goto-next-match)
+            ("M" #'evil-mc-make-and-goto-prev-match)
+            ("n" #'evil-mc-skip-and-goto-next-match)
+            ("N" #'evil-mc-skip-and-goto-prev-match)
+            ("J" #'evil-mc-make-cursor-move-next-line)
+            ("K" #'evil-mc-make-cursor-move-prev-line)
+            ("z" #'+multiple-cursors/evil-mc-toggle-cursor-here)
+            ("r" #'+multiple-cursors/evil-mc-undo-cursor)
+            ("R" #'evil-mc-undo-all-cursors)
+            ("p" #'+multiple-cursors/evil-mc-toggle-cursors)
+            ("q" #'evil-mc-resume-cursors "quit" :color blue)
+            ("<escape>" #'evil-mc-resume-cursors "quit" :color blue)))
     )
