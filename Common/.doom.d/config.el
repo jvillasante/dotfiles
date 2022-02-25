48daa02abb5e8b3c9f74d6fb2371957cf1e42ba0
Julio C. Villasante <jvillasantegomez@gmail.com>
2022-02-25 08:15:14 -0500

    Some changes to lsp-mode and lsp-ui

--- Common/.doom.d/config.el
+++ Common/.doom.d/config.el
@@ -195,19 +195,23 @@
         (lsp--render-element (concat "```rust\n" sig "\n```"))))
 
 (after! lsp-mode
-    (setq
-        lsp-restart 'ignore
-        lsp-headerline-breadcrumb-enable nil
-        lsp-enable-symbol-highlighting t
-        lsp-enable-indentation nil
-        lsp-eldoc-enable-hover t
-        lsp-eldoc-render-all nil
-        lsp-signature-render-documentation nil
-        lsp-signature-auto-activate nil
-        lsp-signature-doc-lines 1
-        lsp-auto-guess-root nil
-        lsp-enable-file-watchers nil
-        lsp-enable-on-type-formatting nil)
+    (setq lsp-auto-guess-root t)
+    (setq lsp-log-io nil)
+    (setq lsp-restart 'ignore)
+    (setq lsp-enable-symbol-highlighting nil)
+    (setq lsp-enable-on-type-formatting nil)
+    (setq lsp-signature-auto-activate nil)
+    (setq lsp-signature-render-documentation nil)
+    (setq lsp-eldoc-hook nil)
+    (setq lsp-modeline-code-actions-enable nil)
+    (setq lsp-modeline-diagnostics-enable nil)
+    (setq lsp-headerline-breadcrumb-enable nil)
+    (setq lsp-semantic-tokens-enable nil)
+    (setq lsp-enable-folding nil)
+    (setq lsp-enable-imenu nil)
+    (setq lsp-enable-snippet nil)
+    (setq read-process-output-max (* 1024 1024)) ;; 1MB
+    (setq lsp-idle-delay 0.5)
 
     ;; Zig
     (setq lsp-zig-zls-executable
@@ -237,22 +241,12 @@
     (after! lsp-clangd (set-lsp-priority! 'clangd 2)))
 
 (after! lsp-ui
-    (setq
-        lsp-ui-sideline-enable nil
-        lsp-ui-sideline-show-diagnostics nil
-        lsp-ui-sideline-show-hover nil
-        lsp-ui-sideline-show-code-actions nil)
-
-    (setq
-        lsp-ui-peek-enable t
-        lsp-ui-peek-always-show t
-        lsp-ui-peek-show-directory t)
-
-    (setq
-        lsp-ui-doc-enable nil)
-
-    (setq
-        lsp-ui-imenu-enable t))
+    (setq lsp-ui-doc-enable nil)
+    (setq lsp-ui-doc-header t)
+    (setq lsp-ui-doc-include-signature t)
+    (setq lsp-ui-doc-border (face-foreground 'default))
+    (setq lsp-ui-sideline-show-code-actions t)
+    (setq lsp-ui-sideline-delay 0.05))
 
 (after! rustic
     (setq
