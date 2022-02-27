be0e8c0303519c862f317da9840fde558975fb8b
Julio C. Villasante <jvillasantegomez@gmail.com>
2022-02-27 08:46:00 -0500

    Changes to zig script

--- Common/.bin/zig.sh
+++ Common/.bin/zig.sh
@@ -2,6 +2,33 @@
 
 . "$(dirname "$0")/common.sh"
 
+update_zig() {
+    local ZIG_DIR="${HOME}/Workspace/Software/zig"
+    if [ ! -d "$ZIG_DIR" ]; then
+        mkdir "$ZIG_DIR"
+    fi
+
+    local ZIG_ZIG_DIR="$ZIG_DIR/zig"
+    if [ -d "$ZIG_ZIG_DIR" ]; then
+        rm -rf "$ZIG_ZIG_DIR"
+    fi
+    mkdir "$ZIG_ZIG_DIR"
+
+    # Get tarball from here: https://ziglang.org/download/
+    local ZIG_TAR_LOCATION="https://ziglang.org/builds/zig-linux-x86_64-0.10.0-dev.1009+4d658f83e.tar.xz"
+    local ZIG_TAR_FILE
+    ZIG_TAR_FILE="$(basename $ZIG_TAR_LOCATION)"
+
+    curl --output-dir "$ZIG_ZIG_DIR" -sS -O "$ZIG_TAR_LOCATION"
+    check $?
+
+    tar --strip-components=1 -xvf "$ZIG_ZIG_DIR/$ZIG_TAR_FILE" --directory "$ZIG_ZIG_DIR"
+    check $?
+
+    rm -f "$ZIG_ZIG_DIR/$ZIG_TAR_FILE"
+    check $?
+}
+
 update_zls() {
     if hash zig 2>/dev/null; then
         local ZIG_DIR="${HOME}/Workspace/Software/zig"
@@ -25,15 +52,20 @@ update_zls() {
 
 while true; do
     PS3="Choose an option: "
-    options=("Update ZLS" "Quit")
+    options=("Update Zig" "Update ZLS" "Quit")
     select opt in "${options[@]}"; do
         case $REPLY in
             1)
+                update_zig
+                hr
+                break
+                ;;
+            2)
                 update_zls
                 hr
                 break
                 ;;
-            2) break 2 ;;
+            3) break 2 ;;
             *) echo "Invalid option '$opt'" >&2 ;;
         esac
     done
