d8c161c97df42b15d6a82579a8d4809556aa86ff
Julio C. Villasante <jvillasantegomez@gmail.com>
2022-03-12 10:53:43 -0500

    Adds skim fuzzy finder instead of fzf

--- make.sh
+++ make.sh
@@ -94,7 +94,7 @@ for file in $files; do
 done
 
 echo ">> Linking common files in $HOME/.config..."
-files="tmux ranger rofi nvim psd i3"
+files="shell tmux ranger rofi nvim psd i3"
 for file in $files; do
     unlink "$HOME/.config/$file"
     ln -s "$DOTFILES_DIR/Common/$file" "$HOME/.config"
