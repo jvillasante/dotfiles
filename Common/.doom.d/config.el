9c24d13ec515fdd9546c3a52e0e8e89d7d6b0ddb
Julio C. Villasante <jvillasantegomez@gmail.com>
2022-03-29 13:08:27 -0400

    Typo on org font

--- Common/.doom.d/config.el
+++ Common/.doom.d/config.el
@@ -459,8 +459,8 @@
         (when (display-graphic-p)
             (let* ((variable-tuple
                        (cond
+                           ((x-list-fonts   "Source Code Pro") '(:font   "Source Code Pro"))
                            ((x-list-fonts   "JetBrains Mono")  '(:font   "JetBrains Mono"))
-                           ((x-list-fonts   "Source Code Pro") '(:font   "Source Pro Pro"))
                            ((x-list-fonts   "Lucida Grande")   '(:font   "Lucida Grande"))
                            ((x-list-fonts   "Verdana")         '(:font   "Verdana"))
                            ((x-family-fonts "Sans Serif")      '(:family "Sans Serif"))
