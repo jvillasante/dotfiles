0a4ab2817bc6a69fb31cff970172c1604083d9a0
Julio C. Villasante <jvillasantegomez@gmail.com>
2022-04-18 18:11:30 -0400

    small change

--- Common/doom/init.el
+++ Common/doom/init.el
@@ -50,8 +50,7 @@
         +all
         +defaults)
     ;;tabs              ; a tab bar for Emacs
-    ;; (treemacs          ; a project drawer, like neotree but cooler
-    ;;     +lsp)
+    ;;treemacs          ; a project drawer, like neotree but cooler
     ;;unicode           ; extended unicode support for various languages
     vc-gutter         ; vcs diff in the fringe
     vi-tilde-fringe   ; fringe tildes to mark beyond EOB
