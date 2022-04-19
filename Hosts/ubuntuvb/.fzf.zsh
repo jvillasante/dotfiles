1e3100bd15b3fccb96043b0809afcff12c1aaeb6
Julio C. Villasante <jvillasantegomez@gmail.com>
2022-04-19 06:27:29 -0400

    removed unused hosts

--- Hosts/ubuntuvb/.fzf.zsh
+++
@@ -1,7 +0,0 @@
-# Auto-completion
-# ---------------
-# source "/etc/bash_completion.d/fzf"
-
-# Key bindings
-# ------------
-[ -f /usr/share/fzf/shell/key-bindings.zsh ] && source /usr/share/fzf/shell/key-bindings.zsh
