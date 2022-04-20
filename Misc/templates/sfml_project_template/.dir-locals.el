c5761b208ed8a4e398eb69a7cc12f84a1221f3bb
Julio C. Villasante <jvillasantegomez@gmail.com>
2022-04-20 09:09:33 -0400

    changes to cpp templates

--- Misc/templates/sfml_project_template/.dir-locals.el
+++ Misc/templates/sfml_project_template/.dir-locals.el
@@ -1,4 +1,8 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
+
+((nil .
+      ((compile-command . "make -j debug"))))
+
 ((c++-mode
   (flycheck-clang-language-standard . "c++20")))
