7a324bda7c3a75852eb8980282fba81a4ae46b65
Julio C. Villasante <jvillasantegomez@gmail.com>
2022-04-23 10:36:35 -0400

    small change

--- Common/.bin/tmuxifier/DmxsWindow.sh
+++ Common/.bin/tmuxifier/DmxsWindow.sh
@@ -15,7 +15,8 @@ else
 
     # Split window into panes.
     split_v 80
-    run_cmd "docker run --rm --interactive --tty --volume $HOME/Workspace/Work/Projects/dmxs:/tmp/sm -u $(id -u "${USER}"):$(id -g "${USER}") sm:latest /bin/bash"
+    run_cmd "docker run --rm --interactive --tty --volume $HOME/Workspace/Work/Projects/dmxs:/tmp/sm \
+        -u $(id -u "${USER}"):$(id -g "${USER}") sm:latest /bin/bash"
     run_cmd "cd /tmp/sm"
 
     # Set active pane.
