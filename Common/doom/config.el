08e231ce9309cf8864cd85d0f9de1947b3472f93
Julio C. Villasante <jvillasantegomez@gmail.com>
2022-05-14 16:24:08 -0400

    whitespace

--- Common/doom/config.el
+++ Common/doom/config.el
@@ -486,7 +486,7 @@
         (when (display-graphic-p)
             (let* ((variable-tuple
                        (cond
-                           ((x-list-fonts   "Iosevka SS09") '(:font   "Iosevka SS09"))
+                           ((x-list-fonts   "Iosevka SS09")      '(:font   "Iosevka SS09"))
                            ((x-list-fonts   "Source Code Pro")   '(:font   "Source Code Pro"))
                            ((x-list-fonts   "JetBrains Mono")    '(:font   "JetBrains Mono"))
                            ((x-list-fonts   "Source Sans Pro")   '(:font   "Source Sans Pro"))
