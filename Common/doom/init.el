72a1c2fa194d5d690d80dd7ef37b2bbebb4b7b34
Julio C. Villasante <jvillasantegomez@gmail.com>
2022-06-11 11:46:00 -0400

    Small change

--- Common/doom/init.el
+++ Common/doom/init.el
@@ -59,8 +59,8 @@
     ;;zen               ; distraction-free coding or writing
 
     :editor
-    (evil             ; come to the dark side, we have cookies
-        +everywhere)
+    (evil +everywhere) ; come to the dark side, we have cookies
+
     file-templates    ; auto-snippets for empty files
     fold              ; (nigh) universal code folding
     (format           ; automated prettiness
