7f62ca495e4fafc1fd391b2d8fa4aacaa8c0bdad
Julio C. Villasante <jvillasantegomez@gmail.com>
2022-06-12 09:23:23 -0400

    Adds Prelude as git submodule

--- Common/.emacs-profiles.el
+++ Common/.emacs-profiles.el
@@ -1,5 +1,6 @@
 (
     ("default" . ((user-emacs-directory . "~/Workspace/Public/dotfiles/.emacs.doom")
                      (env . (("DOOMDIR" . "~/Workspace/Public/dotfiles/Common/doom")))))
+    ("prelude" . ((user-emacs-directory . "~/Workspace/Public/dotfiles/prelude")))
     ("rational" . ((user-emacs-directory . "~/Workspace/Public/dotfiles/.emacs.rational")
                       (env . (("RATIONAL_EMACS_HOME" . "~/Workspace/Public/dotfiles/Common/rational-emacs"))))))
