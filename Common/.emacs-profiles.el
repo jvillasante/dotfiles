6e3945ad30a9cce6eb60a7ec178ec7daa64fdbb9
Julio C. Villasante <jvillasantegomez@gmail.com>
2022-06-20 14:32:04 -0400

    Remove prelude emacs

--- Common/.emacs-profiles.el
+++ Common/.emacs-profiles.el
@@ -1,8 +1,6 @@
 (
-    ("personal" . ((user-emacs-directory . "~/Workspace/Public/dotfiles/Common/emacs/personal")))
     ("default" . ((user-emacs-directory . "~/Workspace/Public/dotfiles/.emacs.doom")
                      (env . (("DOOMDIR" . "~/Workspace/Public/dotfiles/Common/emacs/doom")))))
-    ("prelude" . ((user-emacs-directory . "~/Workspace/Public/dotfiles/.emacs.prelude")
-                     (env . (("PRELUDE_PERSONAL_DIR" . "~/Workspace/Public/dotfiles/Common/emacs/prelude")))))
+    ("personal" . ((user-emacs-directory . "~/Workspace/Public/dotfiles/Common/emacs/personal")))
     ("rational" . ((user-emacs-directory . "~/Workspace/Public/dotfiles/.emacs.rational")
                       (env . (("RATIONAL_EMACS_HOME" . "~/Workspace/Public/dotfiles/Common/emacs/rational"))))))
