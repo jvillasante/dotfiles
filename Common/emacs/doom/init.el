95b00ad5b45aa351981874df9242c7c269fa110f
Julio C. Villasante <jvillasantegomez@gmail.com>
2022-07-20 15:58:56 -0400

    Remove tree-sitter

--- Common/emacs/doom/init.el
+++ Common/emacs/doom/init.el
@@ -108,7 +108,7 @@
     ;;taskrunner        ; taskrunner for all your projects
     ;;terraform         ; infrastructure as code
     tmux              ; an API for interacting with tmux
-    tree-sitter       ; syntax and parsing, sitting in a tree...
+    ;;tree-sitter       ; syntax and parsing, sitting in a tree...
     ;;upload            ; map local to remote projects via ssh/ftp
 
     :os
@@ -118,7 +118,7 @@
     :lang
     ;;agda              ; types of types of types of types...
     ;;beancount         ; mind the GAAP
-    (cc +lsp +tree-sitter)           ; C/C++/Obj-C madness
+    (cc +lsp -tree-sitter)           ; C/C++/Obj-C madness
     ;;clojure           ; java with a lisp
     ;; common-lisp       ; if you've seen one lisp, you've seen them all
     ;;coq               ; proofs-as-programs
@@ -168,7 +168,7 @@
     rest              ; Emacs as a REST client
     ;;rst               ; ReST in peace
     ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-    (rust +lsp +tree-sitter)         ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+    (rust +lsp -tree-sitter)         ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
     ;;scala             ; java, but good
     (scheme +guile)   ; a fully conniving family of lisps
     (sh +lsp)                ; she sells {ba,z,fi}sh shells on the C xor
