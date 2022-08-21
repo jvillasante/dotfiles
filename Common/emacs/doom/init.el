854c61fd8c4e06b92e41d3c22a76533c5d94c3e4
Julio C. Villasante <jvillasantegomez@gmail.com>
2022-08-21 08:49:01 -0400

    Trying out tree-sitter

--- Common/emacs/doom/init.el
+++ Common/emacs/doom/init.el
@@ -106,7 +106,7 @@
     ;;taskrunner        ; taskrunner for all your projects
     ;;terraform         ; infrastructure as code
     tmux              ; an API for interacting with tmux
-    ;;tree-sitter       ; syntax and parsing, sitting in a tree...
+    tree-sitter       ; syntax and parsing, sitting in a tree...
     ;;upload            ; map local to remote projects via ssh/ftp
 
     :os
@@ -116,7 +116,7 @@
     :lang
     ;;agda              ; types of types of types of types...
     ;;beancount         ; mind the GAAP
-    (cc +lsp -tree-sitter)           ; C/C++/Obj-C madness
+    (cc +lsp +tree-sitter)           ; C/C++/Obj-C madness
     ;;clojure           ; java with a lisp
     ;; common-lisp       ; if you've seen one lisp, you've seen them all
     ;;coq               ; proofs-as-programs
@@ -143,7 +143,7 @@
     ;;idris             ;
     json              ; At least it ain't XML
     ;;(java +lsp) ; the poster child for carpal tunnel syndrome
-    javascript        ; all(hope(abandon(ye(who(enter(here))))))
+    (javascript +lsp +tree-sitter)        ; all(hope(abandon(ye(who(enter(here))))))
     ;;julia             ; a better, faster MATLAB
     ;;kotlin            ; a better, slicker Java(Script)
     latex             ; writing papers in Emacs has never been so fun
@@ -165,7 +165,7 @@
     rest              ; Emacs as a REST client
     ;;rst               ; ReST in peace
     ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-    (rust +lsp -tree-sitter)         ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+    (rust +lsp +tree-sitter)         ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
     ;;scala             ; java, but good
     (scheme +guile)   ; a fully conniving family of lisps
     (sh +lsp)                ; she sells {ba,z,fi}sh shells on the C xor
