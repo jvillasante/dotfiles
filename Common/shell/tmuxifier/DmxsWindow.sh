fb83040e8020c615b37dbd7c3c9cce518fc95a3b
Julio C. Villasante <jvillasantegomez@gmail.com>
2022-10-15 12:41:13 -0400

    Updates tmuxifier dmxs window

--- Common/shell/tmuxifier/DmxsWindow.sh
+++ Common/shell/tmuxifier/DmxsWindow.sh
@@ -5,9 +5,11 @@ if tmux list-windows -F '#W' | grep -q "^$WINDOW_NAME\$"; then
     select_window $WINDOW_NAME
     select_pane 2
 else
+    WINDOW_ROOT="$HOME/Workspace/Work/Projects/dmxs"
+
     # Set window root path. Default is `$session_root`.
     # Must be called before `new_window`.
-    window_root "$HOME/Workspace/Work/Projects/dmxs"
+    window_root "$WINDOW_ROOT"
 
     # Create new window. If no argument is given, window name will be based on
     # layout file name.
@@ -15,8 +17,7 @@ else
 
     # Split window into panes.
     split_v 80
-    run_cmd "docker run --rm --interactive --tty --volume $HOME/Workspace/Work/Projects/dmxs:/tmp/sm \
-        -u jenkins -w /tmp/sm --name dmxs sm:latest /bin/bash"
+    run_cmd "docker run --rm --interactive --tty --volume $WINDOW_ROOT:/tmp/sm -u jenkins -w /tmp/sm --name dmxs sm:latest /bin/bash"
 
     # Set active pane.
     select_pane 2
