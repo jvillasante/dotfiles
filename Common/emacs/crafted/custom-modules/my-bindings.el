37b01561ffc6c588270de480c48bb485424b882d
Julio C. Villasante <jvillasantegomez@gmail.com>
2022-10-30 12:15:53 -0400

    Removing `general.el` and using default emacs bindings

--- Common/emacs/crafted/custom-modules/my-bindings.el
+++ Common/emacs/crafted/custom-modules/my-bindings.el
@@ -1,195 +1,189 @@
 ;;; my-bindings.el --- Custom Bindings -*- lexical-binding: t; -*-
+;;;
+;;; Links:
+;;; https://www.masteringemacs.org/article/mastering-key-bindings-emacs
 
 ;;; Commentary:
 ;;; Bindings Customizations
 
 ;;; Code:
-(crafted-package-install-package 'general)
-(progn
-    ;; * Global Keybindings
-    ;; `general-define-key' acts like `global-set-key' when :keymaps is not
-    ;; specified (because ":keymaps 'global" is the default)
-    ;; kbd is not necessary and arbitrary amount of key def pairs are allowed
-    (general-define-key
-        ;;; general keys
-        "C-x C-m" 'execute-extended-command ; ALT may or may not be available
-        "C-c C-m" 'execute-extended-command ; ALT may or may not be available
-        "C-c u" 'browse-url-at-point ; simple browse url
-        "C-x k" 'kill-this-buffer ; kill buffer without prompt
-        "C-x K" 'kill-buffer ; prompt for buffer to kill
-        "M-/" 'hippie-expand ; use hippie-expand instead of debbrev
-        "C-x S" '+my/save-all ; save some buffers without prompt
-        "C-z" nil ; suspend frame should go away
-        "C-x C-z" nil ; same
 
-        ;;; for some reason, crafted-emacs remaps C-s to `consult-line'
-        "C-s" 'isearch-forward
+;; some globals
+(global-set-key (kbd "C-x C-m") 'execute-extended-command) ; ALT may or may not be available
+(global-set-key (kbd "C-c C-m") 'execute-extended-command) ; ALT may or may not be available
+(global-set-key (kbd "C-c u") 'browse-url-at-point) ; simple browse url
+(global-set-key (kbd "C-x k") 'kill-this-buffer) ; kill buffer without prompt
+(global-set-key (kbd "C-x K") 'kill-buffer) ; prompt for buffer to kill
+(global-set-key (kbd "M-/") 'hippie-expand) ; use hippie-expand instead of debbrev
+(global-set-key (kbd "C-x S") '+my/save-all) ; save some buffers without prompt
+(global-set-key (kbd "C-z") nil) ; suspend frame should go away
+(global-set-key (kbd "C-x C-z") nil) ; same
 
-        ;;; upcase, downcase and capitalize
-        "M-u" 'upcase-dwim
-        "M-l" 'downcase-dwim
-        "M-c" 'capitalize-dwim
+;; for some reason, crafted-emacs remaps C-s to `consult-line'
+(global-set-key (kbd "C-s") 'isearch-forward)
 
-        ;;; hippie-expand is a better dabbrev
-        [remap dabbrev-expand] 'hippie-expand
+;; upcase, downcase and capitalize
+(global-set-key (kbd "M-u") 'upcase-dwim)
+(global-set-key (kbd "M-l") 'downcase-dwim)
+(global-set-key (kbd "M-c") 'capitalize-dwim)
 
-        ;;; fill-unfill
-        [remap fill-paragraph] '+my/fill-or-unfill
-        "M-Q"                  '+my/unfill-paragraph
-        "C-M-Q"                '+my/unfill-region
+;; hippie-expand is a better dabbrev
+(define-key global-map [remap dabbrev-expand] 'hippie-expand)
 
-        ;;; avy
-        "C-." 'avy-goto-char-timer
+;; fill-unfill
+(define-key global-map [remap fill-paragraph] '+my/fill-or-unfill)
+(global-set-key (kbd "M-Q") '+my/unfill-paragraph)
+(global-set-key (kbd "C-M-Q") '+my/unfill-region)
 
-        ;;; ace-window
-        ;; [remap other-window] 'ace-window ; better other window
+;; avy
+(global-set-key (kbd "C-.") 'avy-goto-char-timer)
 
-        ;;; anzu
-        "M-%" 'anzu-query-replace
-        "C-M-%" 'anzu-query-replace-regexp
+;; ace-window
+;; (define-key global-map [remap other-window] 'ace-window)
 
-        ;;; expand-region
-        "C-=" 'er/expand-region
-        "C--" 'er/contract-region
+;; anzu
+(global-set-key (kbd "M-%") 'anzu-query-replace)
+(global-set-key (kbd "C-M-%") 'anzu-query-replace-regexp)
 
-        ;;; magit
-        "C-x g" 'magit-status
+;; expand-region
+(global-set-key (kbd "C-=") 'er/expand-region)
+(global-set-key (kbd "C--") 'er/contract-region)
 
-        ;;; embark
-        "C-;" 'embark-act        ; pick some comfortable binding
-        "C-M-;" 'embark-dwim     ; good alternative: "M-."
-        "C-h B" 'embark-bindings ; alternative for `describe-bindings'
+;; magit
+(global-set-key (kbd "C-x g") 'magit-status)
 
-        ;;; consult
-        ;; C-c bindings (mode-specific-map)
-        "C-c h" 'consult-history
-        "C-c m" 'consult-mode-command
-        ;; "C-c k" 'consult-kmacro
-        ;; C-x bindings (Ctl-x-map)
-        "C-x M-:" 'consult-complex-command     ; orig. repeat-complex-command
-        "C-x b" 'consult-buffer                ; orig. switch-to-buffer
-        "C-x 4 b" 'consult-buffer-other-window ; orig. switch-to-buffer-other-window
-        "C-x 5 b" 'consult-buffer-other-frame  ; orig. switch-to-buffer-other-frame
-        "C-x r b" 'consult-bookmark            ; orig. bookmark-jump
-        "C-x p b" 'consult-project-buffer      ; orig. project-switch-to-buffer
-        ;; Custom M-# bindings for fast register access
-        "M-#" 'consult-register-load
-        "M-'" 'consult-register-store          ; orig. abbrev-prefix-mark (unrelated)
-        "C-M-#" 'consult-register
-        ;; Other custom bindings
-        "M-y" 'consult-yank-pop                ; orig. yank-pop
-        "<help> a" 'consult-apropos            ; orig. apropos-command
-        ;; M-g bindings (goto-map)
-        "M-g e" 'consult-compile-error
-        "M-g f" 'consult-flymake               ; Alternative: consult-flycheck
-        "M-g g" 'consult-goto-line             ; orig. goto-line
-        "M-g M-g" 'consult-goto-line           ; orig. goto-line
-        "M-g o" 'consult-outline               ; Alternative: consult-org-heading
-        "M-g m" 'consult-mark
-        "M-g k" 'consult-global-mark
-        "M-g i" 'consult-imenu
-        "M-g I" 'consult-imenu-multi
-        ;; M-s bindings (search-map)
-        "M-s d" 'consult-find
-        "M-s D" 'consult-locate
-        "M-s g" 'consult-grep
-        "M-s G" 'consult-git-grep
-        "M-s r" 'consult-ripgrep
-        "M-s l" 'consult-line
-        "M-s L" 'consult-line-multi
-        "M-s m" 'consult-multi-occur
-        "M-s k" 'consult-keep-lines
-        "M-s u" 'consult-focus-lines
-        ;; Isearch integration
-        "M-s e" 'consult-isearch-history
+;; embark
+(global-set-key (kbd "C-;") 'embark-act)        ; pick some comfortable binding
+(global-set-key (kbd "C-M-;") 'embark-dwim)     ; good alternative: "M-."
+(global-set-key (kbd "C-h B") 'embark-bindings) ; alternative for `describe-bindings'
 
-        ;; crux
-        ;; "C-c o" 'crux-open-with
-        ;; "C-c u" 'crux-view-url
-        "C-o" 'crux-smart-open-line
-        "M-o" 'crux-smart-open-line-above
-        "C-x C-r" 'crux-recentf-find-file
-        "C-c f" 'crux-recentf-find-file
-        "C-c F" 'crux-recentf-find-directory
-        ;; "C-c n" 'crux-cleanup-buffer-or-region
-        "C-M-z" 'crux-indent-defun
-        "C-c e" 'crux-eval-and-replace
-        "C-c w" 'crux-swap-windows
-        "C-c D" 'crux-delete-file-and-buffer
-        "C-c r" 'crux-rename-buffer-and-file
-        "C-c t" 'crux-visit-term-buffer
-        "C-c k" 'crux-kill-other-buffers
-        "C-c TAB" 'crux-indent-rigidly-and-copy-to-clipboard
-        "C-c I" 'crux-find-user-custom-file
-        "C-c S" 'crux-find-shell-init-file
-        "C-^" 'crux-top-join-line
-        "C-c s" 'crux-ispell-word-then-abbrev
-        "C-k" 'crux-smart-kill-line
-        ;; "C-<backspace>" 'crux-kill-line-backwards
-        "C-x 4 t" 'crux-transpose-windows
-        "C-x C-u" 'crux-upcase-region
-        "C-x C-l" 'crux-downcase-region
-        "C-x M-c" 'crux-capitalize-region
-        [remap move-beginning-of-line] 'crux-move-beginning-of-line
-        [shift return] 'crux-smart-open-line
-        [control shift return] 'crux-smart-open-line-above
-        [remap kill-whole-line] 'crux-kill-whole-line)
+;; consult
+;; C-c bindings (mode-specific-map)
+(global-set-key (kbd "C-c h") 'consult-history)
+(global-set-key (kbd "C-c m") 'consult-mode-command)
+;; (global-set-key (kbd "C-c k") 'consult-kmacro)
+;; C-x bindings (Ctl-x-map)
+(global-set-key (kbd "C-x M-:") 'consult-complex-command)     ; orig. repeat-complex-command
+(global-set-key (kbd "C-x b") 'consult-buffer)                ; orig. switch-to-buffer
+(global-set-key (kbd "C-x 4 b") 'consult-buffer-other-window) ; orig. switch-to-buffer-other-window
+(global-set-key (kbd "C-x 5 b") 'consult-buffer-other-frame)  ; orig. switch-to-buffer-other-frame
+(global-set-key (kbd "C-x r b") 'consult-bookmark)            ; orig. bookmark-jump
+(global-set-key (kbd "C-x p b") 'consult-project-buffer)      ; orig. project-switch-to-buffer
+;; Custom M-# bindings for fast register access
+(global-set-key (kbd "M-#") 'consult-register-load)
+(global-set-key (kbd "M-'") 'consult-register-store)          ; orig. abbrev-prefix-mark (unrelated)
+(global-set-key (kbd "C-M-#") 'consult-register)
+;; Other custom bindings
+(global-set-key (kbd "M-y") 'consult-yank-pop)                ; orig. yank-pop
+(global-set-key (kbd "<help> a") 'consult-apropos)            ; orig. apropos-command
+;; M-g bindings (goto-map)
+(global-set-key (kbd "M-g e") 'consult-compile-error)
+(global-set-key (kbd "M-g f") 'consult-flymake)               ; Alternative: consult-flycheck
+(global-set-key (kbd "M-g g") 'consult-goto-line)             ; orig. goto-line
+(global-set-key (kbd "M-g M-g") 'consult-goto-line)           ; orig. goto-line
+(global-set-key (kbd "M-g o") 'consult-outline)               ; Alternative: consult-org-heading
+(global-set-key (kbd "M-g m") 'consult-mark)
+(global-set-key (kbd "M-g k") 'consult-global-mark)
+(global-set-key (kbd "M-g i") 'consult-imenu)
+(global-set-key (kbd "M-g I") 'consult-imenu-multi)
+;; M-s bindings (search-map)
+(global-set-key (kbd "M-s d") 'consult-find)
+(global-set-key (kbd "M-s D") 'consult-locate)
+(global-set-key (kbd "M-s g") 'consult-grep)
+(global-set-key (kbd "M-s G") 'consult-git-grep)
+(global-set-key (kbd "M-s r") 'consult-ripgrep)
+(global-set-key (kbd "M-s l") 'consult-line)
+(global-set-key (kbd "M-s L") 'consult-line-multi)
+(global-set-key (kbd "M-s m") 'consult-multi-occur)
+(global-set-key (kbd "M-s k") 'consult-keep-lines)
+(global-set-key (kbd "M-s u") 'consult-focus-lines)
+;; Isearch integration
+(global-set-key (kbd "M-s e") 'consult-isearch-history)
 
-    ;; * Prefix Keybindings
-    ;; :prefix can be used to prevent redundant specification of prefix keys
-    (general-define-key :prefix "C-c c" ; code
-        "f" 'format-all-buffer)
+;; crux
+;; (global-set-key (kbd "C-c o") 'crux-open-with)
+;; (global-set-key (kbd "C-c u") 'crux-view-url)
+(global-set-key (kbd "C-o") 'crux-smart-open-line)
+(global-set-key (kbd "M-o") 'crux-smart-open-line-above)
+(global-set-key (kbd "C-x C-r") 'crux-recentf-find-file)
+(global-set-key (kbd "C-c f") 'crux-recentf-find-file)
+(global-set-key (kbd "C-c F") 'crux-recentf-find-directory)
+;; (global-set-key (kbd "C-c n") 'crux-cleanup-buffer-or-region)
+(global-set-key (kbd "C-M-z") 'crux-indent-defun)
+(global-set-key (kbd "C-c e") 'crux-eval-and-replace)
+(global-set-key (kbd "C-c w") 'crux-swap-windows)
+(global-set-key (kbd "C-c D") 'crux-delete-file-and-buffer)
+(global-set-key (kbd "C-c r") 'crux-rename-buffer-and-file)
+(global-set-key (kbd "C-c t") 'crux-visit-term-buffer)
+(global-set-key (kbd "C-c k") 'crux-kill-other-buffers)
+(global-set-key (kbd "C-c TAB") 'crux-indent-rigidly-and-copy-to-clipboard)
+(global-set-key (kbd "C-c I") 'crux-find-user-custom-file)
+(global-set-key (kbd "C-c S") 'crux-find-shell-init-file)
+(global-set-key (kbd "C-^") 'crux-top-join-line)
+(global-set-key (kbd "C-c s") 'crux-ispell-word-then-abbrev)
+(global-set-key (kbd "C-k") 'crux-smart-kill-line)
+;; (global-set-key (kbd "C-<backspace>") 'crux-kill-line-backwards)
+(global-set-key (kbd "C-x 4 t") 'crux-transpose-windows)
+(global-set-key (kbd "C-x C-u") 'crux-upcase-region)
+(global-set-key (kbd "C-x C-l") 'crux-downcase-region)
+(global-set-key (kbd "C-x M-c") 'crux-capitalize-region)
+(define-key global-map [remap move-beginning-of-line] 'crux-move-beginning-of-line)
+(define-key global-map [remap kill-whole-line] 'crux-kill-whole-line)
 
-    (general-define-key :prefix "C-c n" ; notes
-        "d" 'deft-find-file
-        "D" 'deft)
+;; prefix: C-c c
+(global-set-key (kbd "C-c c f") 'format-all-buffer)
 
-    (general-define-key :prefix "C-c o" ; open
-        "c" 'calc
-        "C" 'quick-calc
-        "e" 'elfeed
-        "p" 'neotree-toggle
-        "v" 'vterm)
+;; prefix: C-c n
+(global-set-key (kbd "C-c n d") 'deft-find-file)
+(global-set-key (kbd "C-c n D") 'deft)
 
-    ;; * Mode Keybindings
-    ;; `general-define-key' is comparable to `define-key' when :keymaps is specified
-    (general-define-key :keymaps 'ctl-x-4-map
-        "t" '+my/toggle-window-split)
+;; prefix: C-c o
+(global-set-key (kbd "C-c o c") 'calc)
+(global-set-key (kbd "C-c o C") 'quick-calc)
+(global-set-key (kbd "C-c o e") 'elfeed)
+(global-set-key (kbd "C-c o p") 'neotree-toggle)
+(global-set-key (kbd "C-c o t") 'vterm)
+(global-set-key (kbd "C-c o e") 'eshell)
 
-    (general-define-key :keymaps 'org-mode-map
-        [remap fill-paragraph] '+my/org-fill-or-unfill)
+;; map: ctl-x-4-map
+(define-key ctl-x-4-map (kbd "t") '+my/toggle-window-split)
 
-    (general-define-key :keymaps 'dired-mode-map
-        "C-c o" 'crux-open-with)
+;; map: org-mode-map
+(with-eval-after-load "org"
+    (define-key org-mode-map [remap fill-paragraph] '+my/org-fill-or-unfill))
 
-    (general-define-key :keymaps 'isearch-mode-map
-        ;; Prevents issue where you have to press backspace twice when
-        ;; trying to remove the first character that fails a search
-        [remap isearch-delete-char] 'isearch-del-char
-        "C-n" 'isearch-repeat-forward ; better navigation
-        "C-p" 'isearch-repeat-backward ; better navigation
-        "M-e" 'consult-isearch-history ; orig. isearch-edit-string
-        "M-s e" 'consult-isearch-history ; orig. isearch-edit-string
-        "M-s l" 'consult-line ; needed by consult-line to detect isearch
-        "M-s L" 'consult-line-multi) ; needed by consult-line to detect isearch
+;; map: dired-mode-map
+(define-key dired-mode-map (kbd "C-c o") 'crux-open-with)
 
-    (general-define-key :keymaps 'ibuffer-mode-map
-        "q" 'kill-this-buffer)
+;; map: isearch-mode-map
+;; Prevents issue where you have to press backspace twice when trying
+;; to remove the first character that fails a search
+(define-key isearch-mode-map [remap isearch-delete-char] 'isearch-del-char)
+(define-key isearch-mode-map (kbd "C-n") 'isearch-repeat-forward) ; better navigation
+(define-key isearch-mode-map (kbd "C-p") 'isearch-repeat-backward) ; better navigation
+(define-key isearch-mode-map (kbd "M-e") 'consult-isearch-history) ; orig. isearch-edit-string
+(define-key isearch-mode-map (kbd "M-s e") 'consult-isearch-history) ; orig. isearch-edit-string
+(define-key isearch-mode-map (kbd "M-s l") 'consult-line) ; needed by consult-line to detect isearch
+(define-key isearch-mode-map (kbd "M-s L") 'consult-line-multi) ; needed by consult-line to detect isearch
 
-    (general-define-key :keymaps 'smartparens-mode-map
-        "C-M-<backspace>" 'sp-backward-kill-sexp)
+;; map: ibuffer-mode-map
+(define-key minibuffer-mode-map (kbd "q") 'kill-this-buffer)
 
-    (general-define-key :keymaps 'vterm-mode-map
-        "M-[" 'vterm-copy-mode
-        "C-y" 'vterm-yank)
+;; map: smartparens-mode-map
+(define-key smartparens-mode-map (kbd "C-M-<backspace>") 'sp-backward-kill-sexp)
 
-    (general-define-key :keymaps 'neotree-mode-map
-        "TAB" 'neotree-stretch-toggle)
+;; map: vterm-mode-map
+(with-eval-after-load "vterm"
+    (define-key vterm-mode-map (kbd "M-[") 'vterm-copy-mode)
+    (define-key vterm-mode-map (kbd "C-y") 'vterm-yank))
 
-    (general-define-key :keymaps 'minibuffer-local-map
-        "M-s" 'consult-history ; orig. next-matching-history-element
-        "M-r" 'consult-history) ; orig. previous-matching-history-element
-    )
+;; map: neotree-mode-map
+(with-eval-after-load "neotree"
+    (define-key neotree-mode-map (kbd "TAB") 'neotree-stretch-toggle))
+
+;; map: minibuffer-local-map
+(define-key minibuffer-local-map (kbd "M-s") 'consult-history) ; orig. next-matching-history-element
+(define-key minibuffer-local-map (kbd "M-r") 'consult-history) ; orig. previous-matching-history-element
 
 (provide 'my-bindings)
 ;;; my-bindings.el ends here
