ca21c87618ca27bd24b52e8c4859241bcd3a66bd
Julio C. Villasante <jvillasantegomez@gmail.com>
2022-10-30 09:56:23 -0400

    crafted-emacs bindings

--- Common/emacs/crafted/custom-modules/my-bindings.el
+++ Common/emacs/crafted/custom-modules/my-bindings.el
@@ -39,29 +39,69 @@
         "C-M-Q"                #'+my/unfill-region
 
         ;;; avy
-        (when (crafted-package-installed-p 'avy)
-            "C-." 'avy-goto-char-timer)
+        "C-." 'avy-goto-char-timer
 
-        ;; undo
-        (when (crafted-package-installed-p 'undo-fu)
-            "C-/" 'undo-fu-only-undo
-            "C-?" 'undo-fu-only-redo)
+        ;;; ace-window
+        ;; [remap other-window] 'ace-window ; better other window
 
-        ;;;; ace-window
-        [remap other-window] 'ace-window ; better other window
-
-        ;;;; anzu
+        ;;; anzu
         "M-%" 'anzu-query-replace
         "C-M-%" 'anzu-query-replace-regexp
 
         ;;; expand-region
-        (when (crafted-package-installed-p 'expand-region)
-            "C-=" 'er/expand-region
-            "C--" 'er/contract-region)
+        "C-=" 'er/expand-region
+        "C--" 'er/contract-region
 
         ;;; magit
-        (when (crafted-package-installed-p 'magit)
-            "C-x g" 'magit-status)
+        "C-x g" 'magit-status
+
+        ;;; embark
+        "C-;" 'embark-act        ; pick some comfortable binding
+        "C-M-;" 'embark-dwim     ; good alternative: "M-."
+        "C-h B" 'embark-bindings ; alternative for `describe-bindings'
+
+        ;;; consult
+        ;; C-c bindings (mode-specific-map)
+        "C-c h" 'consult-history
+        "C-c m" 'consult-mode-command
+        ;; "C-c k" 'consult-kmacro
+        ;; C-x bindings (Ctl-x-map)
+        "C-x M-:" 'consult-complex-command     ; orig. repeat-complex-command
+        "C-x b" 'consult-buffer                ; orig. switch-to-buffer
+        "C-x 4 b" 'consult-buffer-other-window ; orig. switch-to-buffer-other-window
+        "C-x 5 b" 'consult-buffer-other-frame  ; orig. switch-to-buffer-other-frame
+        "C-x r b" 'consult-bookmark            ; orig. bookmark-jump
+        "C-x p b" 'consult-project-buffer      ; orig. project-switch-to-buffer
+        ;; Custom M-# bindings for fast register access
+        "M-#" 'consult-register-load
+        "M-'" 'consult-register-store          ; orig. abbrev-prefix-mark (unrelated)
+        "C-M-#" 'consult-register
+        ;; Other custom bindings
+        "M-y" 'consult-yank-pop                ; orig. yank-pop
+        "<help> a" 'consult-apropos            ; orig. apropos-command
+        ;; M-g bindings (goto-map)
+        "M-g e" 'consult-compile-error
+        "M-g f" 'consult-flymake               ; Alternative: consult-flycheck
+        "M-g g" 'consult-goto-line             ; orig. goto-line
+        "M-g M-g" 'consult-goto-line           ; orig. goto-line
+        "M-g o" 'consult-outline               ; Alternative: consult-org-heading
+        "M-g m" 'consult-mark
+        "M-g k" 'consult-global-mark
+        "M-g i" 'consult-imenu
+        "M-g I" 'consult-imenu-multi
+        ;; M-s bindings (search-map)
+        "M-s d" 'consult-find
+        "M-s D" 'consult-locate
+        "M-s g" 'consult-grep
+        "M-s G" 'consult-git-grep
+        "M-s r" 'consult-ripgrep
+        "M-s l" 'consult-line
+        "M-s L" 'consult-line-multi
+        "M-s m" 'consult-multi-occur
+        "M-s k" 'consult-keep-lines
+        "M-s u" 'consult-focus-lines
+        ;; Isearch integration
+        "M-s e" 'consult-isearch-history
 
         ;; crux
         ;; "C-c o" 'crux-open-with
--- Common/emacs/crafted/custom-modules/my-ide.el
+++ Common/emacs/crafted/custom-modules/my-ide.el
@@ -5,9 +5,57 @@
 
 ;;; Code:
 
+;; Project.el enhancements
+(with-eval-after-load 'project
+    (defcustom project-root-markers
+        '("Cargo.toml" "compile_commands.json" "compile_flags.txt"
+             "project.clj" ".git" "deps.edn" "shadow-cljs.edn")
+        "Files or directories that indicate the root of a project."
+        :type '(repeat string)
+        :group 'project)
+
+    (defun project-root-p (path)
+        "Check if the current PATH has any of the project root markers."
+        (catch 'found
+            (dolist (marker project-root-markers)
+                (when (file-exists-p (concat path marker))
+                    (throw 'found marker)))))
+
+    (defun project-find-root (path)
+        "Search up the PATH for `project-root-markers'."
+        (when-let ((root (locate-dominating-file path #'project-root-p)))
+            (cons 'transient (expand-file-name root))))
+    (add-to-list 'project-find-functions #'project-find-root)
+
+    (defun project-save-some-buffers (&optional arg)
+        "Save some modified file-visiting buffers in the current project.
+
+Optional argument ARG (interactively, prefix argument) non-nil
+means save all with no questions."
+        (interactive "P")
+        (let* ((project-buffers (project-buffers (project-current)))
+                  (pred (lambda () (memq (current-buffer) project-buffers))))
+            (funcall-interactively #'save-some-buffers arg pred)))
+
+    (define-advice project-compile (:around (fn) save-project-buffers)
+        "Only ask to save project-related buffers."
+        (let* ((project-buffers (project-buffers (project-current)))
+                  (compilation-save-buffers-predicate
+                      (lambda () (memq (current-buffer) project-buffers))))
+            (funcall fn)))
+
+    (define-advice recompile (:around (fn &optional edit-command) save-project-buffers)
+        "Only ask to save project-related buffers if inside a project."
+        (if (project-current)
+            (let* ((project-buffers (project-buffers (project-current)))
+                      (compilation-save-buffers-predicate
+                          (lambda () (memq (current-buffer) project-buffers))))
+                (funcall fn edit-command))
+            (funcall fn edit-command))))
+
 ;; Eglot
 (setc eldoc-echo-area-use-multiline-p t)
-(when (crafted-package-installed-p 'eglot)
+(with-eval-after-load "eglot"
     (with-eval-after-load "eglot"
         (setc eglot-extend-to-xref t)
         (setc eglot-ignored-server-capabilities
