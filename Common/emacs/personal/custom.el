be82024c71d7fe1c7b6e6c0bc68268d542bbfa85
Julio C. Villasante <jvillasantegomez@gmail.com>
2023-03-26 06:19:54 -0400

    Adds custom.el to git
    Problem:
    
    Solution:
    
    Notes:

---
+++ Common/emacs/personal/custom.el
@@ -0,0 +1,23 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(connection-local-criteria-alist
+   '(((:application eshell)
+      eshell-connection-default-profile)))
+ '(connection-local-profile-alist
+   '((eshell-connection-default-profile
+      (eshell-path-env-list))))
+ '(help-at-pt-display-when-idle '(flymake-overlay) nil (help-at-pt))
+ '(help-at-pt-timer-delay 0.5)
+ '(package-selected-packages
+   '(dired-sidebar all-the-icons-dired diredfl dired multiple-cursors dwim-shell-command whole-line-or-region expand-region hydra undo-tree super-save avy exec-path-from-shell editorconfig persistent-scratch crux ibuffer-vc vterm rainbow-delimiters helpful isearch pdf-tools elfeed fancy-compilation apheleia js2-mode rustic go-mode markdown-mode web-mode yaml-mode csv-mode adoc-mode c++-mode elispfl elisp-mode deft org-agenda org-capture org-superstar hl-todo git-gutter magit yasnippet-snippets yasnippet cape corfu-terminal corfu embark-consult embark consult-dir consult marginalia orderless vertico anzu which-key minions all-the-icons modus-themes no-littering)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(fancy-compilation-default-face ((t (:inherit nil :background unspecified))))
+ '(flymake-error ((((class color)) (:underline "red"))))
+ '(flymake-warning ((((class color)) (:underline "yellow")))))
