49066e063e7b2f6193fa50d10940a0a265bc4395
Julio C. Villasante <jvillasantegomez@gmail.com>
2023-03-28 14:45:10 -0400

    Removes comments

--- Common/emacs/personal/lisp/my-init-bindings.el
+++ Common/emacs/personal/lisp/my-init-bindings.el
@@ -65,48 +65,6 @@
 (global-set-key (kbd "C-h F") 'helpful-function)
 (global-set-key (kbd "C-h K") 'describe-keymap) ; alternative for `describe-bindings'
 
-;; ;; consult
-;; ;; C-c bindings (mode-specific-map)
-;; (global-set-key (kbd "C-c h") 'consult-history)
-;; (global-set-key (kbd "C-c m") 'consult-mode-command)
-;; ;; (global-set-key (kbd "C-c k") 'consult-kmacro)
-;; ;; C-x bindings (Ctl-x-map)
-;; (global-set-key (kbd "C-x M-:") 'consult-complex-command)     ; orig. repeat-complex-command
-;; (global-set-key (kbd "C-x b") 'consult-buffer)                ; orig. switch-to-buffer
-;; (global-set-key (kbd "C-x 4 b") 'consult-buffer-other-window) ; orig. switch-to-buffer-other-window
-;; (global-set-key (kbd "C-x 5 b") 'consult-buffer-other-frame)  ; orig. switch-to-buffer-other-frame
-;; (global-set-key (kbd "C-x r b") 'consult-bookmark)            ; orig. bookmark-jump
-;; (global-set-key (kbd "C-x p b") 'consult-project-buffer)      ; orig. project-switch-to-buffer
-;; ;; Custom M-# bindings for fast register access
-;; (global-set-key (kbd "M-#") 'consult-register-load)
-;; (global-set-key (kbd "M-'") 'consult-register-store)          ; orig. abbrev-prefix-mark (unrelated)
-;; (global-set-key (kbd "C-M-#") 'consult-register)
-;; ;; Other custom bindings
-;; (global-set-key (kbd "M-y") 'consult-yank-pop)                ; orig. yank-pop
-;; (global-set-key (kbd "<help> a") 'consult-apropos)            ; orig. apropos-command
-;; ;; M-g bindings (goto-map)
-;; (global-set-key (kbd "M-g e") 'consult-compile-error)
-;; (global-set-key (kbd "M-g g") 'consult-goto-line)             ; orig. goto-line
-;; (global-set-key (kbd "M-g M-g") 'consult-goto-line)           ; orig. goto-line
-;; (global-set-key (kbd "M-g o") 'consult-outline)               ; Alternative: consult-org-heading
-;; (global-set-key (kbd "M-g m") 'consult-mark)
-;; (global-set-key (kbd "M-g k") 'consult-global-mark)
-;; (global-set-key (kbd "M-g i") 'consult-imenu)
-;; (global-set-key (kbd "M-g I") 'consult-imenu-multi)
-;; ;; M-s bindings (search-map)
-;; (global-set-key (kbd "M-s d") 'consult-find)
-;; (global-set-key (kbd "M-s D") 'consult-locate)
-;; (global-set-key (kbd "M-s g") 'consult-grep)
-;; (global-set-key (kbd "M-s G") 'consult-git-grep)
-;; (global-set-key (kbd "M-s r") 'consult-ripgrep)
-;; (global-set-key (kbd "M-s l") 'consult-line)
-;; (global-set-key (kbd "M-s L") 'consult-line-multi)
-;; (global-set-key (kbd "M-s m") 'consult-multi-occur)
-;; (global-set-key (kbd "M-s k") 'consult-keep-lines)
-;; (global-set-key (kbd "M-s u") 'consult-focus-lines)
-;; ;; Isearch integration
-;; (global-set-key (kbd "M-s e") 'consult-isearch-history)
-
 ;; crux
 ;; (global-set-key (kbd "C-c o") 'crux-open-with)
 ;; (global-set-key (kbd "C-c u") 'crux-view-url)
