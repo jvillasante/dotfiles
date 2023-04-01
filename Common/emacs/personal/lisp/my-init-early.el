914334194a7f6d8d5070539aacd9198fff59b611
Julio C. Villasante <jvillasantegomez@gmail.com>
2023-04-01 16:44:29 -0400

    Changing completion ignore case defaults

--- Common/emacs/personal/lisp/my-init-early.el
+++ Common/emacs/personal/lisp/my-init-early.el
@@ -125,9 +125,9 @@
 (setq make-backup-files nil) ; Do not use backup files (filename~)
 (setq create-lockfiles nil) ; Do not use lock files (.#filename)
 (setq line-spacing 0.1) ;; line spacing
-(setq completion-ignore-case t)                ;; ignore case
-(setq read-file-name-completion-ignore-case t) ;; ignore case when completing filenames
-(setq read-buffer-completion-ignore-case t)    ;; ignore case whem completing buffers
+(setq completion-ignore-case nil)              ;; do NOT ignore case by default
+(setq read-file-name-completion-ignore-case t) ;; ... but, ignore case when completing filenames
+(setq read-buffer-completion-ignore-case t)    ;; ... and, ignore case whem completing buffers
 
 (when IS-MAC
     (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
