281d33396617ae82f7767dca30c353f9df7071cb
Julio C. Villasante <jvillasantegomez@gmail.com>
2023-04-02 09:35:49 -0400

    Adds no-littering auto-save
    Problem:
    
    Solution:
    
    Notes:

--- Common/emacs/personal/init.el
+++ Common/emacs/personal/init.el
@@ -27,7 +27,12 @@
       use-package-expand-minimally t)
 
 ;; no-littering needs to come first
-(use-package no-littering)
+(use-package no-littering
+    :init
+    ;; no-littering doesn't set this by default so we must place
+    ;; auto save files in the same path as it uses for sessions
+    (setq auto-save-file-name-transforms
+        `((".*" ,(no-littering-expand-var-file-name "auto-save/") t))))
 
 ;; files and paths constants
 (defconst my/home-path (expand-file-name "~/"))
