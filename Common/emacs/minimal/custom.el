92c9aa406c701498ee8c439a61a186bf3a099a88
Julio C. Villasante <jvillasantegomez@gmail.com>
2023-05-27 07:34:35 -0400

    changes to minimal

--- Common/emacs/minimal/custom.el
+++ Common/emacs/minimal/custom.el
@@ -3,14 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(connection-local-criteria-alist
-      '(((:application eshell)
-            eshell-connection-default-profile)))
- '(connection-local-profile-alist
-      '((eshell-connection-default-profile
-            (eshell-path-env-list))))
  '(package-selected-packages
-      '(avy editorconfig org-contrib markdown-mode yaml-mode typescript-mode rust-mode lua-mode json-mode go-mode diff-hl magit corfu consult vertico)))
+      '(consult vertico corfu magit diff-hl go-mode json-mode lua-mode rust-mode typescript-mode yaml-mode markdown-mode org-contrib editorconfig avy)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
