7912abb7ea45bbc97ccfc753ef227a12cb66f7b3
Julio C. Villasante <jvillasantegomez@gmail.com>
2023-06-12 09:32:48 -0400

    changes to nyxt config

--- Common/nyxt/config.lisp
+++ Common/nyxt/config.lisp
@@ -1,6 +1,17 @@
-;; Flathub: $HOME/.var/app/engineer.atlas.Nyxt/config/nyxt
+(in-package #:nyxt-user)
 
-;; emacs bindings
+;; Emacs please
 (define-configuration buffer
     ((default-modes
          (pushnew 'nyxt/mode/emacs:emacs-mode %slot-value%))))
+
+;; Enable blocker-mode
+(define-configuration web-buffer
+    ((default-modes
+         (pushnew 'nyxt/mode/blocker:blocker-mode %slot-value%))))
+
+(define-configuration browser
+    ((restore-session-on-startup-p nil)))
+
+(define-configuration document-buffer
+    ((search-always-auto-complete-p nil)))
