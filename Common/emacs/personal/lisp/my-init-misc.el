bd32ec582af3d9bd5d4f8faf1914042341f1a08e
Julio C. Villasante <jvillasantegomez@gmail.com>
2023-07-14 17:47:13 -0400

    add some bindings

--- Common/emacs/personal/lisp/my-init-misc.el
+++ Common/emacs/personal/lisp/my-init-misc.el
@@ -34,7 +34,8 @@
     (defun my/electric-indent-local-mode-maybe ()
         "Enable `electric-indent-local-mode' if appropriate."
         (unless (or (eq major-mode 'fundamental-mode)
-                    (eq major-mode 'text-mode))
+                    (eq major-mode 'text-mode)
+                    (eq major-mode 'conf-mode))
             (electric-indent-local-mode 1)))
     :init
     (setq-default electric-indent-chars '(?\n ?\^?))
