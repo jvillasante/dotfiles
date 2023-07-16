e24e77a07c47b9ef6182ef8161586c42458afb43
Julio C. Villasante <jvillasantegomez@gmail.com>
2023-07-16 09:53:14 -0400

    Magit display function

--- Common/emacs/personal/lisp/my-init-vcs.el
+++ Common/emacs/personal/lisp/my-init-vcs.el
@@ -4,23 +4,14 @@
 (setq vc-follow-symlinks t)
 
 (use-package magit
-    :init
-    (setq git-commit-summary-max-length 50
-        magit-diff-refine-hunk t ; show granular diffs in selected hunk.
-        ;; Don't autosave repo buffers. This is too magical, and
-        ;; saving can trigger a bunch of unwanted side-effects, like
-        ;; save hooks and formatters. Trust the user to know what
-        ;; they're doing.
-        magit-save-repository-buffers nil
-        magit-define-global-key-bindings nil)
-    :hook
-    ((git-commit-mode . (lambda () (setq-local fill-column 72))))
+    :hook ((git-commit-mode . (lambda () (setq-local fill-column 72))))
     :config
-    (add-to-list 'display-buffer-alist
-        '("magit:" ;; the main magit dashboard
-             ;; don't create new window if there is one magit window
-             ;; and create new tab if there isn't one magit window.
-             (display-buffer-reuse-window display-buffer-same-window))))
+    (setq
+        magit-display-buffer-function 'magit-display-buffer-same-window-except-diff-v1
+        git-commit-summary-max-length 50
+        magit-diff-refine-hunk t ; show granular diffs in selected hunk.
+        magit-save-repository-buffers nil
+        magit-define-global-key-bindings nil))
 
 (use-package git-gutter
     :commands (git-gutter:revert-hunk git-gutter:stage-hunk)
