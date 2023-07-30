a68ab60cee4959cea3600d948f8e8833ddece5dd
Julio C. Villasante <jvillasantegomez@gmail.com>
2023-07-30 14:19:24 -0400

    use-package change

--- Common/emacs/personal/init.el
+++ Common/emacs/personal/init.el
@@ -18,13 +18,12 @@
 (add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/") t)
 (package-initialize)
 (unless package-archive-contents (package-refresh-contents))
-(unless (package-installed-p 'use-package) (package-install 'use-package))
 
 ;; bootstrap use-package
-(require 'use-package)
+(eval-when-compile (require 'use-package))
 (setq use-package-verbose t
-      use-package-always-ensure t
-      use-package-expand-minimally t)
+    use-package-always-ensure t
+    use-package-expand-minimally t)
 
 ;; no-littering needs to come first
 (use-package no-littering)
