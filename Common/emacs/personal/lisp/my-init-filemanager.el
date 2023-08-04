33929129135550d67e66adeeb2604638030569b0
Julio C. Villasante <jvillasantegomez@gmail.com>
2023-08-04 06:45:21 -0400

    Adds dired-sidebar binding

--- Common/emacs/personal/lisp/my-init-filemanager.el
+++ Common/emacs/personal/lisp/my-init-filemanager.el
@@ -74,11 +74,11 @@
     (set-face-attribute 'diredfl-dir-name nil :bold t))
 
 (use-package dired-sidebar
-    :init
-    (add-hook 'dired-sidebar-mode-hook
-        (lambda ()
-            (unless (file-remote-p default-directory)
-                (auto-revert-mode))))
+    :bind (("C-x C-n" . dired-sidebar-toggle-sidebar))
+    :init (add-hook 'dired-sidebar-mode-hook
+              (lambda ()
+                  (unless (file-remote-p default-directory)
+                      (auto-revert-mode))))
     :config
     (push 'toggle-window-split dired-sidebar-toggle-hidden-commands)
     (push 'rotate-windows dired-sidebar-toggle-hidden-commands)
