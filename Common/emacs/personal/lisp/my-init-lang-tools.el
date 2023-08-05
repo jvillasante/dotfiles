ab2f42ecac2d8aeb252e51729dbcb1093068f50d
Julio C. Villasante <jvillasantegomez@gmail.com>
2023-08-05 10:42:54 -0400

    format changes...

--- Common/emacs/personal/lisp/my-init-lang-tools.el
+++ Common/emacs/personal/lisp/my-init-lang-tools.el
@@ -25,8 +25,8 @@
     :ensure nil ;; emacs built-in
     :config
     (add-to-list 'display-buffer-alist
-        '("^\\*eldoc for" display-buffer-at-bottom
-             (window-height . 4)))
+                 '("^\\*eldoc for" display-buffer-at-bottom
+                   (window-height . 4)))
     (setq eldoc-echo-area-use-multiline-p nil)
     (setq eldoc-documentation-strategy 'eldoc-documentation-compose-eagerly))
 
@@ -36,27 +36,27 @@
     (defun my/eglot-eldoc ()
         ;; Show flymake diagnostics first.
         (setq eldoc-documentation-functions
-            (cons #'flymake-eldoc-function
-                (remove #'flymake-eldoc-function eldoc-documentation-functions)))
+              (cons #'flymake-eldoc-function
+                    (remove #'flymake-eldoc-function eldoc-documentation-functions)))
 
         ;; Show all eldoc feedback.
         (setq eldoc-documentation-strategy #'eldoc-documentation-compose-eagerly))
 
     :hook ((eglot-managed-mode . my/eglot-eldoc)
-              (c-mode . eglot-ensure)
-              (c++-mode . eglot-ensure)
-              (rustic-mode . eglot-ensure)
-              (zig-mode . eglot-ensure)
-              (js-mode . eglot-ensure)
-              (python-mode . eglot-ensure)
-              (go-mode . eglot-ensure)
-              (sql-mode . eglot-ensure))
+           (c-mode . eglot-ensure)
+           (c++-mode . eglot-ensure)
+           (rustic-mode . eglot-ensure)
+           (zig-mode . eglot-ensure)
+           (js-mode . eglot-ensure)
+           (python-mode . eglot-ensure)
+           (go-mode . eglot-ensure)
+           (sql-mode . eglot-ensure))
     :init
     (setq eglot-extend-to-xref t)
     (setq eglot-autoshutdown t)
     (setq read-process-output-max (* 1024 1024))
     (setq eglot-ignored-server-capabilities
-        (quote (:documentFormattingProvider :documentRangeFormattingProvider :inlayHintProvider)))
+          (quote (:documentFormattingProvider :documentRangeFormattingProvider :inlayHintProvider)))
 
     :config
     (add-to-list 'eglot-stay-out-of 'eldoc-documentation-strategy)
@@ -64,73 +64,86 @@
     ;; Setting the workspace configuration for every buffer, this can also be
     ;; done as dir-local variables for project/directory.
     (setq-default eglot-workspace-configuration
-        '(:gopls (:staticcheck t :usePlaceholders t)
-             :rust-analyzer (:check (:command "clippy"))))
+                  '(:gopls (:staticcheck t :usePlaceholders t)
+                           :rust-analyzer (:check (:command "clippy"))))
 
     ;; python
     (add-to-list 'eglot-server-programs
-        '(python-mode . ("pyright-langserver" "--stdio")))
+                 '(python-mode . ("pyright-langserver" "--stdio")))
 
     ;; js
     (add-to-list 'eglot-server-programs
-        '(js-mode . ("typescript-language-server" "--stdio")))
+                 '(js-mode . ("typescript-language-server" "--stdio")))
 
     ;; web
     (add-to-list 'eglot-server-programs
-        '(web-mode . ("typescript-language-server" "--stdio")))
+                 '(web-mode . ("typescript-language-server" "--stdio")))
 
     ;; sql
     (add-to-list 'eglot-server-programs
-        '(sql-mode . ("sqls")))
+                 '(sql-mode . ("sqls")))
 
     ;; go
     (add-to-list 'eglot-server-programs
-        '(go-mode . ("gopls")))
+                 '(go-mode . ("gopls")))
 
     ;; rust
     (add-to-list 'eglot-server-programs
-        '(rustic-mode . ("rust-analyzer")))
+                 '(rustic-mode . ("rust-analyzer")))
 
     ;; zig
     (add-to-list 'eglot-server-programs
-        '(zig-mode . (expand-file-name "zig/zls/zig-out/bin/zls" my/software-path)))
+                 '(zig-mode . (expand-file-name "zig/zls/zig-out/bin/zls" my/software-path)))
 
     ;; C++
     (add-to-list 'eglot-server-programs
-        '((c-mode c++-mode)
-             . ("clangd"
-                   "-j=8"
-                   "--log=error"
-                   "--malloc-trim"
-                   "--background-index"
-                   "--clang-tidy"
-                   "--cross-file-rename"
-                   "--completion-style=detailed"
-                   "--pch-storage=memory"
-                   "--header-insertion=never"
-                   "--header-insertion-decorators=0"))))
+                 '((c-mode c++-mode)
+                   . ("clangd"
+                      "-j=8"
+                      "--log=error"
+                      "--malloc-trim"
+                      "--background-index"
+                      "--clang-tidy"
+                      "--cross-file-rename"
+                      "--completion-style=detailed"
+                      "--pch-storage=memory"
+                      "--header-insertion=never"
+                      "--header-insertion-decorators=0"))))
 
 ;; cmake
 (use-package cmake-mode)
 
 ;; apheleia : Good code is automatically formatted
 (use-package apheleia
-    :preface
-    (defun my/disable-apheleia ()
-        (apheleia-mode -1))
+    :preface (defun my/disable-apheleia ()
+                 (apheleia-mode -1))
     :hook ((html-mode-hook . my/disable-apheleia))
-    :init (apheleia-global-mode +1))
+    :bind ("C-c t a" . apheleia-mode)
+    :init (apheleia-global-mode)
+    :config
+    ;; Set custom formatting commands
+    (dolist (formatter-cmd '((shfmt    . ("shfmt" "-i" "4" "-ci" "-kp" "-sr"))
+                             (zigfmt   . ("zig" "fmt" (or (buffer-file-name) (buffer-name))))
+                             (fourmolu . ("fourmolu" "--indentation" "2" "--stdin-input-file"
+                                          (or (buffer-file-name) (buffer-name))))))
+        (add-to-list #'apheleia-formatters formatter-cmd))
+
+    ;; Set custom formatters for modes
+    (dolist (formatter-mode '((emacs-lisp-mode . lisp-indent)
+                              (clojure-mode . lisp-indent)
+                              (zig-mode     . zigfmt)
+                              (haskell-mode . fourmolu)))
+        (add-to-list #'apheleia-mode-alist formatter-mode)))
 
 (use-package compile
     :ensure nil ; Emacs built in
-    :custom
-    (compilation-always-kill t))
+    :custom (compilation-always-kill t))
 
 (use-package fancy-compilation
     :commands (fancy-compilation-mode)
     :init
     (custom-set-faces
-        '(fancy-compilation-default-face ((t (:inherit nil :background unspecified)))))
+     '(fancy-compilation-default-face ((t (:inherit nil :background unspecified)))))
     (with-eval-after-load 'compile
         (fancy-compilation-mode)))
 
