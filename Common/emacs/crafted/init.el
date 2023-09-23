99fccde34295f0b499a07b6acbc80968158b3e0e
Julio C. Villasante <jvillasantegomez@gmail.com>
2023-09-23 08:59:46 -0400

    Adds crafted emacs

---
+++ Common/emacs/crafted/init.el
@@ -0,0 +1,67 @@
+;;; init.el --- -*- lexical-binding: t -*-
+;;; Commentary:
+;;
+;;; Code:
+
+;;; Initial phase.
+
+;; Load the custom file if it exists.  Among other settings, this will
+;; have the list `package-selected-packages', so we need to load that
+;; before adding more packages.  The value of the `custom-file'
+;; variable must be set appropriately, by default the value is nil.
+;; This can be done here, or in the early-init.el file.
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (and custom-file
+           (file-exists-p custom-file))
+    (load custom-file nil 'nomessage))
+
+;; Adds crafted-emacs modules to the `load-path', sets up a module
+;; writing template, sets the `crafted-emacs-home' variable.
+(load (expand-file-name ".emacs.crafted/modules/crafted-init-config" my/dotfiles-path))
+
+;;; Packages phase
+
+;; Collect list of packages to install.  Do not just blindly copy this
+;; list, instead think about what you need and see if there is a
+;; module which provides the list of packages needed.  This phase is
+;; not needed if manage the installed packages with Guix or Nix.  It
+;; is also not needed if you do not need Crafted Emacs to install
+;; packages for a module, for example,
+;; `crafted-mastering-emacs-config' does not require any packages to
+;; be installed.
+(require 'crafted-completion-packages)
+(require 'crafted-ide-packages)
+(require 'crafted-ui-packages)
+(require 'crafted-writing-packages)
+
+;; Install the packages listed in the `package-selected-packages' list.
+(package-install-selected-packages :noconfirm)
+
+;;; Configuration phase
+
+;; Some example modules to configure Emacs. Don't blindly copy these,
+;; they are here for example purposes.  Find the modules which work
+;; for you and add them here.
+(require 'crafted-defaults-config)
+(require 'crafted-startup-config)
+(require 'crafted-completion-config)
+(require 'crafted-ide-config)
+(require 'crafted-startup-config)
+(require 'crafted-ui-config)
+(require 'crafted-updates-config)
+(require 'crafted-writing-config)
+
+;;; Optional configuration
+
+;; Profile emacs startup
+(defun crafted-startup-example/display-startup-time ()
+    "Display the startup time after Emacs is fully initialized."
+    (message "Crafted Emacs loaded in %s."
+             (emacs-init-time)))
+(add-hook 'emacs-startup-hook #'crafted-startup-example/display-startup-time)
+
+;; Set default coding system (especially for Windows)
+(set-default-coding-systems 'utf-8)
+
+(provide 'init)
+;;; init.el ends here
