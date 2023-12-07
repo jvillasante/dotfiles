f378e5dc423c3e39ded2470b5bb80cc95b7844c8
Julio C. Villasante <jvillasantegomez@gmail.com>
2023-12-07 18:35:50 -0500

    Removing vertico key-bindings

--- Common/emacs/minimal/init.el
+++ Common/emacs/minimal/init.el
@@ -33,8 +33,8 @@
 
 ;; Improve directory navigation
 (with-eval-after-load 'vertico
-    (define-key vertico-map (kbd "RET") #'vertico-directory-enter)
-    (define-key vertico-map (kbd "DEL") #'vertico-directory-delete-word)
+    ;; (define-key vertico-map (kbd "RET") #'vertico-directory-enter)
+    ;; (define-key vertico-map (kbd "DEL") #'vertico-directory-delete-word)
     (define-key vertico-map (kbd "M-d") #'vertico-directory-delete-char))
 
 ;;; Extended completion utilities
