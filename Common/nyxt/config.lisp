916cad88219620cc3b4800f87d1f55d5ef4f8180
Julio C. Villasante <jvillasantegomez@gmail.com>
2023-12-10 09:38:04 -0500

    nyxt browser

--- Common/nyxt/config.lisp
+++ Common/nyxt/config.lisp
@@ -1,17 +1,25 @@
 (in-package #:nyxt-user)
 
 ;; Emacs please
-(define-configuration (web-buffer prompt-buffer panel-buffer
-                                  nyxt/mode/editor:editor-buffer)
-    ((default-modes (pushnew 'nyxt/mode/emacs:emacs-mode %slot-value%))))
+(define-configuration buffer
+    ((default-modes
+         (pushnew 'nyxt/mode/emacs:emacs-mode %slot-value%))))
+
+;; Keybindings
+(define-configuration input-buffer
+    ((override-map
+      (let ((map (make-keymap "override-map")))
+          (define-key map "M-x" 'execute-command "C-space" 'nothing)))))
 
 ;; Enable blocker-mode
 (define-configuration web-buffer
     ((default-modes
          (pushnew 'nyxt/mode/blocker:blocker-mode %slot-value%))))
 
+;; start fresh
 (define-configuration browser
     ((restore-session-on-startup-p nil)))
 
+;; do not complete searches
 (define-configuration document-buffer
     ((search-always-auto-complete-p nil)))
