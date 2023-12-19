02718cabb6cdfd27883e26d7a7bc60531e7b840d
Julio C. Villasante <jvillasantegomez@gmail.com>
2023-12-19 07:47:32 -0500

    adds transient menu to isearch...

--- Common/emacs/personal/lisp/my-init-misc.el
+++ Common/emacs/personal/lisp/my-init-misc.el
@@ -48,6 +48,73 @@
 
 (use-package isearch
     :ensure nil ;; emacs built-in
+    :requires transient
+    :preface
+    (transient-define-prefix my--isearch-menu ()
+        [["Edit Search String"
+          ("e"
+           "Edit the search string (recursive)"
+           isearch-edit-string
+           :transient nil)
+          ("w"
+           "Pull next word or character word from buffer"
+           isearch-yank-word-or-char
+           :transient nil)
+          ("s"
+           "Pull next symbol or character from buffer"
+           isearch-yank-symbol-or-char
+           :transient nil)
+          ("l"
+           "Pull rest of line from buffer"
+           isearch-yank-line
+           :transient nil)
+          ("y"
+           "Pull string from kill ring"
+           isearch-yank-kill
+           :transient nil)
+          ("t"
+           "Pull thing from buffer"
+           isearch-forward-thing-at-point
+           :transient nil)]
+         ["Replace"
+          ("q"
+           "Start ‘query-replace’"
+           isearch-query-replace
+           :if-nil buffer-read-only
+           :transient nil)
+          ("x"
+           "Start ‘query-replace-regexp’"
+           isearch-query-replace-regexp
+           :if-nil buffer-read-only
+           :transient nil)]]
+        [["Toggle"
+          ("X"
+           "Toggle regexp searching"
+           isearch-toggle-regexp
+           :transient nil)
+          ("S"
+           "Toggle symbol searching"
+           isearch-toggle-symbol
+           :transient nil)
+          ("W"
+           "Toggle word searching"
+           isearch-toggle-word
+           :transient nil)
+          ("F"
+           "Toggle case fold"
+           isearch-toggle-case-fold
+           :transient nil)
+          ("L"
+           "Toggle lax whitespace"
+           isearch-toggle-lax-whitespace
+           :transient nil)]
+         ["Misc"
+          ("o"
+           "occur"
+           isearch-occur
+           :transient nil)]])
+    :bind
+    ("<f2>" . my--isearch-menu)
     :init
     (setq isearch-resume-in-command-history t) ; use history for isearch as well
     (setq search-whitespace-regexp ".*?") ; isearch convenience, space matches anything (non-greedy)
