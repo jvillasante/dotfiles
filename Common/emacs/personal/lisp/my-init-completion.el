a146288fdda6a3a064cf8eec79f0177e120d92da
Julio C. Villasante <jvillasantegomez@gmail.com>
2024-01-09 16:51:31 -0500

    cape changes (also password-store)

--- Common/emacs/personal/lisp/my-init-completion.el
+++ Common/emacs/personal/lisp/my-init-completion.el
@@ -220,15 +220,42 @@
     (corfu-popupinfo-mode))
 
 (use-package cape
+    ;; Bind dedicated completion commands
+    :bind (("C-c p p"  . completion-at-point) ;; capf
+           ("C-c p t"  . complete-tag)        ;; etags
+           ("C-c p d"  . cape-dabbrev)        ;; or dabbrev-completion
+           ("C-c p h"  . cape-history)
+           ("C-c p f"  . cape-file)
+           ("C-c p k"  . cape-keyword)
+           ("C-c p s"  . cape-elisp-symbol)
+           ("C-c p e"  . cape-elisp-block)
+           ("C-c p a"  . cape-abbrev)
+           ("C-c p l"  . cape-line)
+           ("C-c p w"  . cape-dict)
+           ("C-c p :"  . cape-emoji)
+           ("C-c p \\" . cape-tex)
+           ("C-c p _"  . cape-tex)
+           ("C-c p ^"  . cape-tex)
+           ("C-c p &"  . cape-sgml)
+           ("C-c p r"  . cape-rfc1345))
     :init
-    (setq cape-dabbrev-min-length 2)
-    (setq cape-symbol-wrapper
-          '((org-mode ?~ ?~)
-            (markdown-mode ?` ?`)
-            (log-edit-mode ?' ?')
-            (message-mode ?' ?')))
-    (dolist (backend '(cape-symbol cape-keyword cape-file cape-history cape-dabbrev))
-        (add-to-list 'completion-at-point-functions backend)))
+    ;; Add to the global default value of `completion-at-point-functions' which is
+    ;; used by `completion-at-point'.  The order of the functions matters, the
+    ;; first function returning a result wins.  Note that the list of buffer-local
+    ;; completion functions takes precedence over the global list.
+    (add-to-list 'completion-at-point-functions #'cape-dabbrev)
+    (add-to-list 'completion-at-point-functions #'cape-file)
+    (add-to-list 'completion-at-point-functions #'cape-elisp-block)
+    ;;(add-to-list 'completion-at-point-functions #'cape-history)
+    ;;(add-to-list 'completion-at-point-functions #'cape-keyword)
+    ;;(add-to-list 'completion-at-point-functions #'cape-tex)
+    ;;(add-to-list 'completion-at-point-functions #'cape-sgml)
+    ;;(add-to-list 'completion-at-point-functions #'cape-rfc1345)
+    ;;(add-to-list 'completion-at-point-functions #'cape-abbrev)
+    ;;(add-to-list 'completion-at-point-functions #'cape-dict)
+    ;;(add-to-list 'completion-at-point-functions #'cape-elisp-symbol)
+    ;;(add-to-list 'completion-at-point-functions #'cape-line)
+    )
 
 (use-package consult
     :bind (;; C-c bindings (mode-specific-map)
