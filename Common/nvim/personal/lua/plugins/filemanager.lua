ab169cdbe48e4603888e49068f0837ec714834b2
Julio C. Villasante <jvillasantegomez@gmail.com>
2024-02-24 09:43:31 -0500

    vim changes...

--- Common/nvim/personal/lua/plugins/filemanager.lua
+++ Common/nvim/personal/lua/plugins/filemanager.lua
@@ -3,6 +3,7 @@ return {
         -- A vim-vinegar like file explorer that lets you edit your filesystem like a normal Neovim buffer.
         "stevearc/oil.nvim",
         priority = 1000,
+        enabled = false,
         opts = {},
         config = function()
             vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
