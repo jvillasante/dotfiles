5f68e0512846397f1fc98868bd0c85933934ed2b
Julio C. Villasante <jvillasantegomez@gmail.com>
2024-02-26 11:35:05 -0500

    better copilot mode

--- Common/emacs/personal/lisp/my-init-ai.el
+++ Common/emacs/personal/lisp/my-init-ai.el
@@ -16,7 +16,6 @@
 
 (use-package copilot
     :hook (prog-mode . copilot-mode)
-    :defer t
     :config
     (define-key copilot-completion-map (kbd "<tab>") 'copilot-accept-completion)
     (define-key copilot-completion-map (kbd "TAB") 'copilot-accept-completion))
