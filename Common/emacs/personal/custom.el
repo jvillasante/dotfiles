60aa8c6d96b7e396edff68dbde72bd1700ea4c9b
Julio C. Villasante <jvillasantegomez@gmail.com>
2024-03-24 13:03:30 -0400

    Adds custom.el to git
    Why:
    
    How:
    
    Tags:

---
+++ Common/emacs/personal/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(anzu-deactivate-region t)
+ '(anzu-mode-lighter "")
+ '(anzu-replace-threshold 50)
+ '(anzu-replace-to-string-separator " => ")
+ '(anzu-search-threshold 1000)
+ '(package-selected-packages
+   '(transient-dwim gptel anzu which-key minions modus-themes dired-sidebar diredfl engine-mode monkeytype devdocs surround wgrep multiple-cursors whole-line-or-region expand-region avy editorconfig crux rainbow-delimiters helpful vundo ibuffer-project dwim-shell-command vterm eshell-prompt-extras elfeed docker pdf-tools password-store rmsbolt fancy-compilation apheleia js2-mode lua-mode json-mode zig-mode rust-mode go-mode markdown-mode yaml-mode csv-mode adoc-mode cmake-mode elisp-demos treesit-auto deft org-superstar hl-todo diff-hl magit langtool yasnippet-snippets jinx marginalia embark-consult embark consult-eglot consult-dir consult cape corfu orderless vertico s no-littering exec-path-from-shell vc-use-package))
+ '(package-vc-selected-packages
+   '((vc-use-package :vc-backend Git :url "https://github.com/slotThe/vc-use-package"))))
