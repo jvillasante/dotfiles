d9b7bba1a0edee58ce1de34017aed7eb712ca0bd
Julio C. Villasante <jvillasantegomez@gmail.com>
2024-04-22 11:30:43 -0400

    SFML Project Template Fix
    Why:
    
    How:
    
    Tags:

--- Misc/templates/sfml_project_template/src/Minimal/source/main.cpp
+++ Misc/templates/sfml_project_template/src/Minimal/source/main.cpp
@@ -4,7 +4,8 @@
 #include <iostream>
 #include <ResourcePath.hpp>
 
-int main() {
+int main()
+{
     std::cout << "SFML Version:" << SFML_VERSION_MAJOR << "." << SFML_VERSION_MINOR << "." << SFML_VERSION_PATCH
               << std::endl;
 
@@ -12,7 +13,8 @@ int main() {
     std::cout << "Current Mode: (" << current_mode.width << 'x' << current_mode.height << ')' << '\n';
 
     std::vector<sf::VideoMode> modes = sf::VideoMode::getFullscreenModes();
-    for (const sf::VideoMode& mode : modes) {
+    for (const sf::VideoMode& mode : modes)
+    {
         std::cout << "  Mode: (" << mode.width << 'x' << mode.height << ')' << '\n';
     }
 
@@ -23,24 +25,25 @@ int main() {
     window.setMouseCursorVisible(false);
 
     sf::Texture mushroomTexture;
-    mushroomTexture.loadFromFile(resource_path() + "media/textures/Mushroom.png");
+    mushroomTexture.loadFromFile(resource_path() + "/media/textures/Mushroom.png");
     sf::Sprite mushroom(mushroomTexture);
     sf::Vector2u size = mushroomTexture.getSize();
     mushroom.setOrigin(static_cast<float>(size.x) / 2, static_cast<float>(size.y) / 2);
     sf::Vector2f increment(8.0F, 8.0F);
     // mushroom.setColor(sf::Color(255, 0, 0, 150));
 
-    while (window.isOpen()) {
+    while (window.isOpen())
+    {
         sf::Event event{};
-        while (window.pollEvent(event)) {
-            switch (event.type) {
+        while (window.pollEvent(event))
+        {
+            switch (event.type)
+            {
             case sf::Event::Closed:
                 window.close();
                 break;
             case sf::Event::KeyPressed:
-                if (event.key.code == sf::Keyboard::Q) {
-                    window.close();
-                }
+                if (event.key.code == sf::Keyboard::Q) { window.close(); }
                 break;
             default:
                 break;
@@ -48,12 +51,14 @@ int main() {
         }
 
         if ((mushroom.getPosition().x + (size.x / 2) > window.getSize().x && increment.x > 0) ||
-            (mushroom.getPosition().x - (size.x / 2) < 0 && increment.x < 0)) {
+            (mushroom.getPosition().x - (size.x / 2) < 0 && increment.x < 0))
+        {
             increment.x = -increment.x; // Reverse the direction on X axis.
         }
 
         if ((mushroom.getPosition().y + (size.y / 2) > window.getSize().y && increment.y > 0) ||
-            (mushroom.getPosition().y - (size.y / 2) < 0 && increment.y < 0)) {
+            (mushroom.getPosition().y - (size.y / 2) < 0 && increment.y < 0))
+        {
             increment.y = -increment.y; // Reverse the direction on Y axis.
         }
 
