71b6a3394fb362d6ed9058583039cdf453bd9116
Julio C. Villasante <jvillasantegomez@gmail.com>
2024-06-20 15:21:21 -0400

    Adds CSV mode

--- Common/emacs/personal/lisp/my-init-langs.el
+++ Common/emacs/personal/lisp/my-init-langs.el
@@ -197,5 +197,26 @@
                                            "setTimeout" "clearTimeout" "setInterval" "clearInterval" "location"
                                            "__dirname" "console" "JSON" "jQuery" "$")))
 
+(use-package csv-mode
+    :ensure nil ;; emacs built-in
+    :preface
+    (require 'color)
+    (defun csv-highlight (&optional separator)
+        (interactive (list (when current-prefix-arg (read-char "Separator: "))))
+        (font-lock-mode 1)
+        (let* ((separator (or separator ?\,))
+               (n (count-matches (string separator) (pos-bol) (pos-eol)))
+               (colors (cl-loop for i from 0 to 1.0 by (/ 2.0 n)
+                                collect (apply #'color-rgb-to-hex
+                                               (color-hsl-to-rgb i 0.3 0.5)))))
+            (cl-loop for i from 2 to n by 2
+                     for c in colors
+                     for r = (format "^\\([^%c\n]+%c\\)\\{%d\\}" separator separator i)
+                     do (font-lock-add-keywords nil `((,r (1 '(face (:foreground ,c)))))))))
+    :init
+    (add-hook 'csv-mode-hook 'csv-highlight)
+    (add-hook 'csv-mode-hook 'csv-align-mode)
+    (add-hook 'csv-mode-hook '(lambda () (interactive) (toggle-truncate-lines nil))))
+
 (provide 'my-init-langs)
 ;;; my-init-langs.el ends here
