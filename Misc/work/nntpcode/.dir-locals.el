e357b1a939b0582987fb5e6f9298fc8309f52a0b
Julio C. Villasante <jvillasantegomez@gmail.com>
2024-06-22 10:25:44 -0400

    disable formatting on `.dir-locals`

--- Misc/work/nntpcode/.dir-locals.el
+++ Misc/work/nntpcode/.dir-locals.el
@@ -1,1 +1,3 @@
-((nil . ((compile-command . "tmux send-keys -t nntpcode.2 C-z 'make' Enter"))))
+((nil .
+      ((compile-command . "tmux send-keys -t nntpcode.2 C-z 'make' Enter")
+       (eval . (apheleia-mode -1)))))
