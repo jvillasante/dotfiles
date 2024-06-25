97792d98698237925e7340ce21981cb21966c989
Julio C. Villasante <jvillasantegomez@gmail.com>
2024-06-25 14:26:02 -0400

    tmux changes...

--- Common/shell/system/custom.sh
+++ Common/shell/system/custom.sh
@@ -174,10 +174,47 @@ if [ "$(uname -s)" = 'Linux' ]; then
 fi
 
 #
-# Execute tmux (alacritty supports this natively)
+# Tmux
 #
-if [ -x "$(command -v tmux)" ] && [ -n "${DISPLAY}" ] && [ -z "${TMUX}" ] && [ -z "$INSIDE_EMACS" ]; then
-    ( tmux attach || tmux new -s Default > /dev/null 2>&1) && exit 0
+
+if type emacs > /dev/null 2> /dev/null; then
+    # A shortcut function for easy tmux navigation.
+    #
+    # tx ls will give a list of active tmux sessions.
+    # tx {session_name} behaves differently depending on whether the user is inside a tmux session.
+    # if outside tmux:
+    #    tx {session_name} will attach to {session_name} if it exists, else will create {session_name}
+    # if inside tmux:
+    #    tx {session_name} will switch to {session_name} if it exists, else will print an error message.
+    tx () {
+        SESSION_NAME=$1
+
+        if [ "$SESSION_NAME" == "ls" ]; then
+            tmux ls
+            return
+        fi
+
+        # If we are 'in' tmux, try to switch to the target session, fail if session doesn't exit.
+        if [ -n "$TMUX" ]; then
+            tmux switch-client -t "$SESSION_NAME"
+            return $?
+        fi
+
+        # If we are outside of tmux, attach to the session if it exists, otherwise create it and attach.
+        tmux ls | grep "^${SESSION_NAME}:" -q
+        if [ "$?" -eq 0 ]; then
+            tmux attach -t "$SESSION_NAME"
+        else
+            tmux new -s "$SESSION_NAME"
+        fi
+    }
+
+    alias txk='tmux kill-session -t'
+
+    # Execute tmux (alacritty supports this natively)
+    if [ -x "$(command -v tmux)" ] && [ -n "${DISPLAY}" ] && [ -z "${TMUX}" ] && [ -z "$INSIDE_EMACS" ]; then
+        ( tmux attach || tmux new -s Default > /dev/null 2>&1) && exit 0
+    fi
 fi
 
 #
