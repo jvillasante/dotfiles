574c221c1478942371ee031cacb9885d276c1c95
Julio C. Villasante <jvillasantegomez@gmail.com>
2024-07-04 13:39:33 -0400

    `defvar' undefined variable

--- Common/emacs/personal/lisp/my-init-transient.el
+++ Common/emacs/personal/lisp/my-init-transient.el
@@ -7,14 +7,17 @@
 
 (use-package casual-calc
     :requires casual-suite
+    :preface (defvar calc-mode-map)
     :bind (:map calc-mode-map ("C-o" . casual-calc-tmenu)))
 
 (use-package casual-info
     :requires casual-suite
+    :preface (defvar Info-mode-map)
     :bind (:map Info-mode-map ("C-o" . casual-info-tmenu)))
 
 (use-package casual-dired
     :requires casual-suite
+    :preface (defvar dired-mode-map)
     :bind (:map dired-mode-map ("C-o" . casual-dired-tmenu)))
 
 ;; (use-package casual-avy
@@ -23,6 +26,7 @@
 
 (use-package casual-isearch
     :requires casual-suite
+    :preface (defvar isearch-mode-map)
     :bind (:map isearch-mode-map ("<f2>" . casual-isearch-tmenu)))
 
 (provide 'my-init-transient)
