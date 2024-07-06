b822b4bd27adaa2b438de36fbaf6123047d10337
Julio C. Villasante <jvillasantegomez@gmail.com>
2024-07-06 09:25:15 -0400

    small change

--- Common/emacs/personal/init.el
+++ Common/emacs/personal/init.el
@@ -60,7 +60,7 @@
 
 ;; Idle garbage collection
 (use-package gcmh
-    :function gcmh-mode
+    :functions gcmh-mode
     :config
     ;; (setopt garbage-collection-messages t)
     ;; (setopt gcmh-high-cons-threshold (* 256 1000 1000))
