dd56e33c2e9fafdc884c96128062a0a73c1f0533
Julio C. Villasante <jvillasantegomez@gmail.com>
2024-07-24 14:24:30 -0400

    emacs minimal changes...

--- Common/emacs/minimal/custom.el
+++ Common/emacs/minimal/custom.el
@@ -3,8 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-      '(avy eat editorconfig org-contrib markdown-mode yaml-mode lua-mode json-mode diff-hl magit corfu consult vertico)))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
