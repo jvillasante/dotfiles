0756468ded15694b6fd5637436d05e6472810bbb
Julio C. Villasante <jvillasantegomez@gmail.com>
2024-07-24 14:20:58 -0400

    get all arguments on shell

--- Common/shell/system/custom.sh
+++ Common/shell/system/custom.sh
@@ -68,7 +68,7 @@ if type emacs > /dev/null 2> /dev/null; then
     function ediff    { emacsclient -c -a '' --eval "(progn (select-frame-set-input-focus (selected-frame)) (ediff-files \"$1\" \"$2\"))"; }
     function edired   { emacsclient -c -a '' --eval "(progn (select-frame-set-input-focus (selected-frame)) (dired \"$1\"))"; }
     function emagit   { emacsclient -c -a '' --eval "(progn (select-frame-set-input-focus (selected-frame)) (magit-status \"$1\"))"; }
-    function eww      { emacsclient -c -a '' --eval '(eww-browse-url "'"$1"'")'; }
+    function eww      { emacsclient -c -a '' --eval "(eww-browse-url \"$*\")"; }
     function ekill    { emacsclient --eval '(save-buffers-kill-emacs)'; }
     function estatus  { systemctl --user status emacs.service; }
     function edebug   { emacs --debug-init --init-directory="$HOME"/Workspace/Public/dotfiles/Common/emacs/personal/ "$@"; }
