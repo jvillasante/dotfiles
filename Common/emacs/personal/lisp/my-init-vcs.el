388dd4d079f9481672cd2c7e25e9ec364a28c52d
Julio C. Villasante <jvillasantegomez@gmail.com>
2024-08-27 09:59:19 -0400

    Small vcs changes...

--- Common/emacs/personal/lisp/my-init-vcs.el
+++ Common/emacs/personal/lisp/my-init-vcs.el
@@ -17,8 +17,7 @@
             (magit-restore-window-configuration)
             (mapc #'kill-buffer buffers)))
     :hook ((git-commit-setup . git-commit-turn-on-auto-fill))
-    :custom (
-             (magit-display-buffer-function 'magit-display-buffer-same-window-except-diff-v1)
+    :custom ((magit-display-buffer-function 'magit-display-buffer-same-window-except-diff-v1)
              (git-commit-summary-max-length 50)
              (git-commit-fill-column 72)
              (magit-diff-refine-hunk t) ; show granular diffs in selected hunk.
@@ -27,20 +26,16 @@
 
 (use-package diff-hl
     :functions global-diff-hl-mode
-    :hook
-    ((magit-pre-refresh . diff-hl-magit-pre-refresh)
-     (magit-post-refresh . diff-hl-magit-post-refresh)
-     (dired-mode . diff-hl-dir-mode))
-    :config
-    (global-diff-hl-mode))
+    :hook ((magit-pre-refresh . diff-hl-magit-pre-refresh)
+           (magit-post-refresh . diff-hl-magit-post-refresh)
+           (dired-mode . diff-hl-dir-mode))
+    :config (global-diff-hl-mode))
 
 (use-package hl-todo
     :defines hl-todo-highlight-punctuation
-    :hook
-    ((prog-mode . hl-todo-mode)
-     (conf-mode . hl-todo-mode))
-    :config
-    (setq hl-todo-highlight-punctuation ":"))
+    :hook ((prog-mode . hl-todo-mode)
+           (conf-mode . hl-todo-mode))
+    :config (setq hl-todo-highlight-punctuation ":"))
 
 (provide 'my-init-vcs)
 ;;; my-init-vcs.el ends here
