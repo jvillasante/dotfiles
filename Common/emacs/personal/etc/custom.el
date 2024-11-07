2faf5b501bd13ac71c9734db7e2914026de1d875
Julio C. Villasante <jvillasantegomez@gmail.com>
2024-11-07 13:25:10 -0500

    custom.el

--- Common/emacs/personal/etc/custom.el
+++ Common/emacs/personal/etc/custom.el
@@ -8,7 +8,21 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
+ '(package-selected-packages
+      '(adoc-mode anzu apheleia avy cape cl-generic cmake-mode consult-dir
+           consult-eglot corfu crux csv-mode deft devdocs-browser diff-hl
+           dired-sidebar diredfl docker dockerfile-mode dwim-shell-command
+           easy-kill eat editorconfig elfeed elisp-demos embark-consult
+           engine-mode erc eshell-prompt-extras exec-path-from-shell expreg
+           faceup fancy-compilation flymake-proselint gcmh go-mode gptel-quick
+           helpful hl-todo ibuffer-project idlwave jinx js2-mode json-mode
+           langtool lua-mode magit map marginalia markdown-mode minions
+           modus-themes monkeytype multiple-cursors no-littering nov ntlm
+           orderless org-superstar password-store pdf-view-restore peg
+           persistent-scratch php-mode python rainbow-delimiters request
+           rust-mode so-long soap-client surround svg tabspaces track-changes
+           tramp use-package verb verilog-mode vertico web-mode wgrep which-key
+           window-tool-bar yaml-mode yasnippet-snippets zig-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
