4abf4619ec411af01761bcbbb00ccdd77b54853d
Julio C. Villasante <jvillasantegomez@gmail.com>
2024-12-02 17:29:24 -0500

    New emacs install

--- Common/emacs/personal/custom.el
+++ Common/emacs/personal/custom.el
@@ -9,27 +9,28 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-      '(adoc-mode anzu apheleia avy cape cl-generic cmake-mode consult-dir
-           consult-eglot consult-xref-stack corfu crux csv-mode deft
-           devdocs-browser diff-hl dired-sidebar diredfl docker dockerfile-mode
-           dwim-shell-command easy-kill editorconfig elfeed elisp-demos
-           embark-consult embrace engine-mode erc eshell-prompt-extras
-           exec-path-from-shell expreg faceup fancy-compilation gcmh go-mode
-           gptel-quick helpful hl-todo ibuffer-project idlwave jinx js2-mode
-           json-mode lua-mode magit map marginalia markdown-mode minions
-           modern-tab-bar modus-themes monkeytype multiple-cursors no-littering
-           nov ntlm orderless org-superstar otpp password-store
-           pdf-view-restore peg persistent-scratch php-mode python
-           rainbow-delimiters request rust-mode shrink-path so-long soap-client
-           surround svg tab-bar-echo-area tabspaces track-changes tramp
-           use-package verb verilog-mode vertico vterm web-mode wgrep which-key
-           window-tool-bar yaml-mode yasnippet-snippets zig-mode))
+      '(adoc-mode anzu apheleia avy bind-key cape cl-generic cl-lib cmake-mode compat
+           consult-dir consult-eglot consult-xref-stack corfu crux csv-mode
+           deft devdocs-browser diff-hl dired-sidebar diredfl docker
+           dockerfile-mode dwim-shell-command easy-kill editorconfig eglot
+           eldoc elfeed elisp-demos embark-consult engine-mode erc
+           eshell-prompt-extras exec-path-from-shell expreg external-completion
+           faceup fancy-compilation flymake gcmh go-mode gptel-quick helpful
+           hl-todo ibuffer-project idlwave jinx js2-mode json-mode jsonrpc
+           let-alist lua-mode magit map marginalia markdown-mode minions
+           modus-themes monkeytype multiple-cursors nadvice no-littering nov
+           ntlm orderless org org-superstar otpp password-store
+           pdf-view-restore peg persistent-scratch php-mode project python
+           rainbow-delimiters request rust-mode seq shrink-path so-long
+           soap-client surround svg track-changes tramp use-package verb
+           verilog-mode vertico vterm web-mode wgrep which-key window-tool-bar
+           xref yaml-mode yasnippet-snippets zig-mode))
  '(package-vc-selected-packages
-      '((consult-xref-stack :url
-            "https://github.com/brett-lempereur/consult-xref-stack" :branch
-            "main")
-           (gptel-quick :url "git@github.com:karthink/gptel-quick.git")
-           (dired-sidebar :url "git@github.com:jojojames/dired-sidebar.git"))))
+      '((gptel-quick :url "git@github.com:karthink/gptel-quick.git")
+           (dired-sidebar :url "git@github.com:jojojames/dired-sidebar.git")
+           (consult-xref-stack :url
+               "https://github.com/brett-lempereur/consult-xref-stack" :branch
+               "main"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
