eb1314f9d663a120349e6deacafc21531754322e
Julio C. Villasante <jvillasantegomez@gmail.com>
2024-12-02 08:12:09 -0500

    back to sane dired listing switches...

--- Common/emacs/personal/etc/elisp/my-init-filemanager.el
+++ Common/emacs/personal/etc/elisp/my-init-filemanager.el
@@ -45,10 +45,8 @@
     ;;  -l : Use long listing format.
     ;;  -h : Human-readable sizes like 1K, 234M, ..
     ;;  -v : Do natural sort .. so the file names starting with . will show up first.
-    ;; (setq dired-listing-switches
-    ;;       "-a -l -h -v --group-directories-first --color=never")
     (setq dired-listing-switches
-          "-AGFhlv --group-directories-first --time-style=long-iso --color=never")
+          "-a -l -h -v --group-directories-first --color=never")
 
     ;; enable some really cool extensions like C-x C-j(dired-jump)
     (if (< emacs-major-version 28)
