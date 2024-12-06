9d180aa7f4db95e2eef8829d0d1221d3afb207bd
Julio C. Villasante <jvillasantegomez@gmail.com>
2024-12-06 13:15:08 -0500

    Remove comment

--- Common/emacs/personal/etc/elisp/my-init-langs.el
+++ Common/emacs/personal/etc/elisp/my-init-langs.el
@@ -32,7 +32,6 @@
                    (clojure    "https://github.com/sogaiu/tree-sitter-clojure" "master" "src")
                    (cmake      "https://github.com/uyha/tree-sitter-cmake")
                    (commonlisp "https://github.com/tree-sitter-grammars/tree-sitter-commonlisp")
-                   ;; (cpp        "https://github.com/tree-sitter/tree-sitter-cpp/" "v0.23.1" "src")
                    (cpp        "https://github.com/tree-sitter/tree-sitter-cpp/" "v0.23.4" "src")
                    (css        "https://github.com/tree-sitter/tree-sitter-css")
                    (dart       "https://github.com/ast-grep/tree-sitter-dart")
