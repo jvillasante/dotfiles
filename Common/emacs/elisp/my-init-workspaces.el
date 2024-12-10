57708933bf418a8935596409aef7e754ed7d8066
Julio C. Villasante <jvillasantegomez@gmail.com>
2024-12-10 13:52:29 -0500

    Adds commented out `activities`

--- Common/emacs/elisp/my-init-workspaces.el
+++ Common/emacs/elisp/my-init-workspaces.el
@@ -3,6 +3,25 @@
 ;;
 ;;; Code:
 
+(use-package activities
+    :disabled t
+    :init
+    (activities-mode)
+    (activities-tabs-mode)
+    ;; Prevent `edebug' default bindings from interfering.
+    (setq edebug-inhibit-emacs-lisp-mode-bindings t)
+
+    :bind
+    (("C-x C-a C-n" . activities-new)
+     ("C-x C-a C-d" . activities-define)
+     ("C-x C-a C-a" . activities-resume)
+     ("C-x C-a C-s" . activities-suspend)
+     ("C-x C-a C-k" . activities-kill)
+     ("C-x C-a RET" . activities-switch)
+     ("C-x C-a b" . activities-switch-buffer)
+     ("C-x C-a g" . activities-revert)
+     ("C-x C-a l" . activities-list)))
+
 ;; otpp : One tab per project, with unique names
 (use-package otpp
     :after tab-bar project
