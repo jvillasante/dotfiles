7a746a1b3cc1f1059b3846dc9de1c45dadf9cf14
Julio C. Villasante <jvillasantegomez@gmail.com>
2024-12-15 14:52:16 -0500

    Better emacs update

--- Common/emacs/emacs.d/custom.el
+++ Common/emacs/emacs.d/custom.el
@@ -8,28 +8,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-      '(activities adoc-mode alert anzu apheleia cape casual-suite cl-generic
-           cmake-mode compiler-explorer consult-dir consult-xref-stack corfu
-           crux csv-mode deft devdocs-browser diff-hl dired-sidebar diredfl
-           disproject docker dockerfile-mode dwim-shell-command easy-kill
-           editorconfig eglot-booster eglot-inactive-regions elfeed elisp-demos
-           embark-consult engine-mode erc eshell-prompt-extras
-           exec-path-from-shell expreg faceup fancy-compilation gcmh go-mode
-           gptel-quick helm helpful hl-todo ht ibuffer-project idlwave jinx
-           js2-mode json-mode lua-mode magit marginalia markdown-mode minions
-           modus-themes monkeytype multiple-cursors no-littering nov ntlm
-           orderless org-msg org-superstar password-store pdf-view-restore peg
-           persistent-scratch php-mode python quelpa-use-package
-           rainbow-delimiters request rmsbolt rust-mode shrink-path so-long
-           soap-client surround svg track-changes tramp verb verilog-mode
-           vertico vterm web-mode wgrep which-key window-tool-bar yaml-mode
-           yasnippet-capf yasnippet-snippets zig-mode))
+ '(package-selected-packages nil)
  '(package-vc-selected-packages
-      '((eglot-booster :url "git@github.com:jdtsmith/eglot-booster.git")
+      '((gptel-quick :url "git@github.com:karthink/gptel-quick.git")
+           (eglot-booster :url "git@github.com:jdtsmith/eglot-booster.git")
            (consult-xref-stack :url
-               "git@github.com:brett-lempereur/consult-xref-stack.git")
-           (gptel-quick :url "git@github.com:karthink/gptel-quick.git"))))
+               "git@github.com:brett-lempereur/consult-xref-stack.git"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
