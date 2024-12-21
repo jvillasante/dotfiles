980a3e002846782ad5bf4874f8b149a35abd4128
Julio C. Villasante <jvillasantegomez@gmail.com>
2024-12-21 08:05:26 -0500

    small org change

--- Common/emacs/elisp/my-init-org.el
+++ Common/emacs/elisp/my-init-org.el
@@ -52,6 +52,7 @@
     (setq org-startup-folded t)
     (setq org-use-fast-todo-selection 'expert)
     (setq org-M-RET-may-split-line '((item . nil)))
+    (setq org-confirm-babel-evaluate nil)
 
     ;; Return or left-click with mouse follows link
     (setq org-return-follows-link t)
