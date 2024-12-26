642e85effbdca3e06dfffca5f946513e66e41cf5
Julio C. Villasante <jvillasantegomez@gmail.com>
2024-12-26 07:51:29 -0500

    cpp project template .dir-locals change

--- Misc/templates/cpp_project_template/.dir-locals.el
+++ Misc/templates/cpp_project_template/.dir-locals.el
@@ -1,3 +1,3 @@
 ;;; .dir-locals --- -*- no-byte-compile: t; lexical-binding: t; -*-
 ((nil .
-      ((compile-command . "make -j$(nproc --ignore=2) debug"))))
+      ((compile-command . "make -j$(nproc --ignore=2)"))))
