24c73e8aeaeca22e7abc39b0895a7f08549f6fe9
Julio C. Villasante <jvillasantegomez@gmail.com>
2025-01-21 12:34:58 -0500

    change...

--- Common/emacs/emacs.d/custom.el
+++ Common/emacs/emacs.d/custom.el
@@ -13,17 +13,17 @@
            consult-notes consult-xref-stack corfu crux csv-mode devdocs-browser
            diff-hl dired-sidebar diredfl docker dockerfile-mode
            dwim-shell-command easy-kill editorconfig eglot-booster
-           eglot-inactive-regions eldoc-diffstat elfeed elisp-demos
-           embark-consult engine-mode erc eshell-prompt-extras
-           exec-path-from-shell expreg faceup fancy-compilation geiser-guile
-           go-mode helpful hl-todo ibuffer-project idlwave jinx js2-mode
-           json-mode lsp-ui lua-mode magit map marginalia minions modus-themes
-           monkeytype multiple-cursors nov ntlm orderless org-superstar otpp
-           password-store pdf-view-restore peg persistent-scratch php-mode
-           python rainbow-delimiters request rmsbolt rust-mode scratch
-           shrink-path so-long soap-client surround svg track-changes tramp
-           trashed use-package verb verilog-mode vertico vterm vundo web-mode
-           wgrep which-key window-tool-bar yaml-mode yasnippet-snippets zig-mode)))
+           eldoc-diffstat elfeed elisp-demos embark-consult engine-mode erc
+           eshell-prompt-extras exec-path-from-shell expreg faceup
+           fancy-compilation geiser-guile go-mode helpful hl-todo
+           ibuffer-project idlwave jinx js2-mode json-mode lsp-ui lua-mode magit
+           map marginalia minions modus-themes monkeytype multiple-cursors nov
+           ntlm orderless org-superstar otpp password-store pdf-view-restore peg
+           persistent-scratch php-mode python rainbow-delimiters request rmsbolt
+           rust-mode scratch shrink-path so-long soap-client surround svg
+           track-changes tramp trashed use-package verb verilog-mode vertico
+           vterm vundo web-mode wgrep which-key window-tool-bar yaml-mode
+           yasnippet-snippets zig-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
