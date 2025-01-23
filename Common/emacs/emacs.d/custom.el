4b35a2cd05a08f84044d02f617441bf7cc7f5fd3
Julio C. Villasante <jvillasantegomez@gmail.com>
2025-01-23 11:53:06 -0500

    small change...

--- Common/emacs/emacs.d/custom.el
+++ Common/emacs/emacs.d/custom.el
@@ -17,11 +17,11 @@
            eshell-prompt-extras exec-path-from-shell expreg external-completion
            faceup fancy-compilation flymake geiser-guile go-mode gptel helpful
            hl-todo ht ibuffer-project idlwave jinx js2-mode json-mode jsonrpc
-           lsp-ui lua-mode lv magit map marginalia minions modus-themes
-           monkeytype multiple-cursors nov ntlm orderless org-superstar otpp
-           password-store pdf-view-restore peg persistent-scratch php-mode
-           python rainbow-delimiters request rmsbolt rust-mode shrink-path
-           so-long soap-client spinner surround svg track-changes tramp trashed
+           lua-mode lv magit map marginalia minions modus-themes monkeytype
+           multiple-cursors nov ntlm orderless org-superstar otpp password-store
+           pdf-view-restore peg persistent-scratch php-mode python
+           rainbow-delimiters request rmsbolt rust-mode shrink-path so-long
+           soap-client spinner surround svg track-changes tramp trashed
            use-package verb verilog-mode vertico vterm vundo web-mode wgrep
            which-key window-tool-bar yaml-mode yasnippet-snippets zig-mode))
  '(package-vc-selected-packages
