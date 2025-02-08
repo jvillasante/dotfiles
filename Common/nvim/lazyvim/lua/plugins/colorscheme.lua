3836218876e76f7fe7590bcbb3d1131e14178fbe
Julio C. Villasante <jvillasantegomez@gmail.com>
2025-02-08 10:48:48 -0500

    nvim changes...

--- Common/nvim/lazyvim/lua/plugins/colorscheme.lua
+++ Common/nvim/lazyvim/lua/plugins/colorscheme.lua
@@ -1,18 +1,22 @@
 return {
-    { -- Theme inspired modus-themes from Emacs
+    -- Theme inspired modus-themes from Emacs
+    {
         "miikanissi/modus-themes.nvim",
-        priority = 1000, -- Make sure to load this before all the other start plugins.
+        lazy = true,
         config = function()
             vim.opt.background = "light"
             -- vim.cmd([[colorscheme modus]]) -- modus_operandi, modus_vivendi
+
+            -- configure highlights by doing something like
+            vim.cmd.hi("Comment gui=none")
         end,
     },
 
-    -- Configure LazyVim to load gruvbox
+    -- Configure LazyVim to load modus-themes
     {
         "LazyVim/LazyVim",
         opts = {
-            colorscheme = "modus",
+            colorscheme = "modus_operandi",
         },
     },
 }
--- Common/nvim/minimal/init.lua
+++ Common/nvim/minimal/init.lua
@@ -1,6 +1,6 @@
 -- [[ Require options ]]
 --  NOTE: Needs to come first *before* loading plugins
-require("options")
+require("config/options")
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
