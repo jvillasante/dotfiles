bff464c87fed548222dc04dfb26832a204bde36f
Julio C. Villasante <jvillasantegomez@gmail.com>
2025-04-26 09:43:35 -0400

    Emacs hide title bar

--- Common/emacs/emacs.d/custom.el
+++ Common/emacs/emacs.d/custom.el
@@ -12,22 +12,21 @@
       '(adoc-mode anzu apheleia avy bind-key cape cl-generic cl-lib cmake-mode compat
            consult-dir consult-notes corfu crux csv-mode dape deadgrep
            devdocs-browser diff-hl dired-sidebar diredfl docker dockerfile-mode
-           dwim-shell-command easy-kill editorconfig eglot eglot-booster eldoc
-           eldoc-diffstat elfeed elisp-demos embark-consult engine-mode erc
+           dwim-shell-command easy-kill editorconfig eglot eldoc eldoc-diffstat
+           elfeed elisp-demos embark-consult engine-mode erc
            eshell-prompt-extras exec-path-from-shell expreg external-completion
            faceup fancy-compilation flymake gcmh geiser-guile go-mode gptel
            helpful hl-todo idlwave jinx js2-mode json-mode jsonrpc ledger-mode
-           let-alist lsp-mode lsp-ui lua-mode magit map marginalia markdown-mode
-           minions modus-themes monkeytype multiple-cursors nadvice nov ntlm
-           orderless org org-superstar password-store pdf-view-restore peg
+           let-alist lua-mode magit map marginalia markdown-mode minions
+           modus-themes monkeytype multiple-cursors nadvice nov ntlm orderless
+           org org-superstar password-store pdf-view-restore peg
            persistent-scratch php-mode project python rainbow-delimiters request
-           rmsbolt rust-mode seq shrink-path so-long soap-client surround svg
-           tabspaces track-changes tramp trashed undo-fu use-package verb
-           verilog-mode vertico vterm vundo web-mode wgrep which-key
-           window-tool-bar xref yaml-mode yasnippet-snippets zig-mode))
+           rust-mode seq shrink-path so-long soap-client surround svg tabspaces
+           track-changes tramp trashed undo-fu use-package verb verilog-mode
+           vertico vterm vundo web-mode wgrep which-key window-tool-bar xref
+           yaml-mode yasnippet-snippets zig-mode))
  '(package-vc-selected-packages
-      '((eglot-booster :url "git@github.com:jdtsmith/eglot-booster.git")
-           (ledger-mode :url "git@github.com:ledger/ledger-mode.git"))))
+      '((ledger-mode :url "git@github.com:ledger/ledger-mode.git"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
