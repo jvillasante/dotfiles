8029c3366a6bee67aa903e904394aac2103ad8ba
Julio C. Villasante <jvillasantegomez@gmail.com>
2025-06-14 09:35:48 -0400

    Simpler emacs from source compilation

--- Common/emacs/emacs.d/custom.el
+++ Common/emacs/emacs.d/custom.el
@@ -34,7 +34,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
+ )
 
 (provide 'custom)
 ;;; custom.el ends here
