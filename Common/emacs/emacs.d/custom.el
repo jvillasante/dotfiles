5420fc6e3a64d181664b08ff5f4fb710902df06f
Julio C. Villasante <jvillasantegomez@gmail.com>
2025-06-29 06:39:47 -0400

    custom...

--- Common/emacs/emacs.d/custom.el
+++ Common/emacs/emacs.d/custom.el
@@ -9,32 +9,32 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-      '(ace-window activities adoc-mode anzu apheleia avy bind-key cape cl-generic
-           cl-lib cmake-mode compat consult-dir consult-notes corfu crux
-           csv-mode dape deadgrep devdocs-browser diff-hl dired-sidebar diredfl
-           docker dockerfile-mode dwim-shell-command easy-kill editorconfig
-           eglot eldoc eldoc-diffstat elfeed elisp-demos embark-consult
-           engine-mode erc exec-path-from-shell expreg external-completion
-           faceup fancy-compilation flymake gcmh geiser-guile go-mode gptel
-           helpful hl-todo idlwave jinx js2-mode json-mode jsonrpc ledger-mode
-           let-alist lsp-ui lua-mode magit map marginalia minions modus-themes
-           monkeytype multiple-cursors nadvice nov ntlm orderless org
-           org-superstar password-store pdf-view-restore peg persistent-scratch
-           php-mode project python rainbow-delimiters request rmsbolt rust-mode
-           seq shrink-path sicp so-long soap-client surround svg track-changes
-           tramp trashed use-package verb verilog-mode vertico vterm vundo
-           web-mode wgrep-deadgrep which-key window-tool-bar xref yaml-mode
-           yasnippet-snippets zig-mode))
+   '(ace-window activities adoc-mode anzu apheleia avy bind-key cape cl-generic
+                cl-lib cmake-mode compat consult-dir consult-notes corfu crux
+                csv-mode dape deadgrep devdocs-browser diff-hl dired-sidebar diredfl
+                docker dockerfile-mode dwim-shell-command easy-kill editorconfig
+                eglot eldoc eldoc-diffstat elfeed elisp-demos embark-consult
+                engine-mode erc exec-path-from-shell expreg external-completion
+                faceup fancy-compilation flymake gcmh geiser-guile go-mode gptel
+                helpful hl-todo idlwave jinx js2-mode json-mode jsonrpc ledger-mode
+                let-alist lsp-ui lua-mode magit map marginalia minions modus-themes
+                monkeytype multiple-cursors nadvice nov ntlm orderless org
+                org-superstar password-store pdf-view-restore peg persistent-scratch
+                php-mode project python rainbow-delimiters request rmsbolt rust-mode
+                seq shrink-path sicp so-long soap-client surround svg track-changes
+                tramp trashed use-package verb verilog-mode vertico vterm
+                vundo web-mode wgrep-deadgrep which-key window-tool-bar xref
+                yaml-mode yasnippet-snippets zig-mode))
  '(package-vc-selected-packages
-      '((gptel :url "git@github.com:karthink/gptel.git")
-           (deadgrep :url "git@github.com:Wilfred/deadgrep.git")
-           (ledger-mode :url "git@github.com:ledger/ledger-mode.git"))))
+   '((gptel :url "git@github.com:karthink/gptel.git")
+     (deadgrep :url "git@github.com:Wilfred/deadgrep.git")
+     (ledger-mode :url "git@github.com:ledger/ledger-mode.git"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
 
 (provide 'custom)
 ;;; custom.el ends here
