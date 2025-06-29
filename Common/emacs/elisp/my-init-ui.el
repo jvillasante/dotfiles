fb26b58cd29e222c14f2f339c709b7ffe46be670
Julio C. Villasante <jvillasantegomez@gmail.com>
2025-06-29 06:39:01 -0400

    Some tab-bar settings

--- Common/emacs/elisp/my-init-ui.el
+++ Common/emacs/elisp/my-init-ui.el
@@ -254,10 +254,11 @@
     :config
     (setq tab-bar-show 1                           ;; hide bar if <= 1 tabs open
           tab-bar-new-button nil                   ;; hide button for creating new tabs
-          tab-bar-close-button-show nil            ;; hide tab close / X button
+          tab-bar-close-button nil                 ;; hide tab close / X button
           tab-bar-history-limit 100                ;; the number of history elements
           tab-bar-new-tab-choice "*scratch*"       ;; buffer to show in new tabs
           tab-bar-tab-hints t                      ;; show tab numbers
+          tab-bar-auto-width nil                   ;; do not resize tabs
           tab-bar-format '(tab-bar-format-tabs     ;; elements to include in bar
                            tab-bar-separator)))
 
