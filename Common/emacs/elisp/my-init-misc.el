06c0504f462877edd8962644c419a1fad0d0c04b
Julio C. Villasante <jvillasantegomez@gmail.com>
2025-08-29 08:32:55 -0400

    some ibuffer changes...

--- Common/emacs/elisp/my-init-misc.el
+++ Common/emacs/elisp/my-init-misc.el
@@ -231,7 +231,27 @@
     :ensure nil  ;; emacs built-in
     :hook (ibuffer-mode . (lambda ()
                               (hl-line-mode)
-                              (ibuffer-auto-mode))))
+                              (ibuffer-auto-mode)))
+    :config
+    (setq ibuffer-expert t)
+    (setq ibuffer-display-summary nil)
+    (setq ibuffer-use-other-window nil)
+    (setq ibuffer-show-empty-filter-groups nil)
+    (setq ibuffer-default-sorting-mode 'filename/process)
+    (setq ibuffer-title-face 'font-lock-doc-face)
+    (setq ibuffer-use-header-line t)
+    (setq ibuffer-default-shrink-to-minimum-size nil)
+    (setq ibuffer-formats
+          '((mark modified read-only locked " "
+                  (name 30 30 :left :elide)
+                  " "
+                  (size 9 -1 :right)
+                  " "
+                  (mode 16 16 :left :elide)
+                  " " filename-and-process)
+            (mark " "
+                  (name 16 -1)
+                  " " filename))))
 
 ;; ibuffer-project : group buffers by custom functions or regexps.
 (use-package ibuffer-project
