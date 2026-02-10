23314872333ee249150cecc1c1dda0963d04ce18
Julio C. Villasante <jvillasantegomez@gmail.com>
2026-02-10 06:11:37 -0500

    pgtk emacs build

--- Common/emacs/emacs.d/custom.el
+++ Common/emacs/emacs.d/custom.el
@@ -8,10 +8,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ibuffer-formats
-      '((mark modified read-only locked " " (name 30 30 :left :elide) " "
-            (size 9 -1 :right) " " (mode 16 16 :left :elide) " "
-            project-file-relative)))
  '(package-selected-packages
       '(adoc-mode anzu apheleia avy bind-key cape chatgpt-shell cl-generic cl-lib
            cmake-mode compat consult-dir consult-notes corfu crux csv-mode dape
@@ -22,14 +18,13 @@
            geiser-guile gnuplot go-mode gptel helpful hl-todo ibuffer-project
            idlwave jinx js2-mode json-mode jsonrpc ledger-mode let-alist
            lua-mode magit map marginalia markdown-mode minions modus-themes
-           monkeytype multiple-cursors nadvice nerd-icons nerd-icons-completion
-           nerd-icons-corfu nerd-icons-dired nov ntlm orderless org
+           monkeytype multiple-cursors nadvice nov ntlm orderless org
            org-superstar otpp package-lint-flymake password-store
            pdf-view-restore peg persistent-scratch php-mode project python
-           rainbow-delimiters rmsbolt rust-mode seq so-long soap-client svg
-           track-changes tramp-hlo trashed use-package verb verilog-mode vertico
-           vim-tab-bar vterm vundo web-mode wgrep which-key window-tool-bar xref
-           yaml-mode yasnippet-snippets zig-mode))
+           rmsbolt rust-mode seq so-long soap-client svg track-changes tramp-hlo
+           trashed use-package verb verilog-mode vertico vim-tab-bar vterm vundo
+           web-mode wgrep which-key window-tool-bar xref yaml-mode
+           yasnippet-snippets zig-mode))
  '(package-vc-selected-packages
       '((chatgpt-shell :url "git@github.com:xenodium/chatgpt-shell.git")
            (gptel :url "git@github.com:karthink/gptel.git")
