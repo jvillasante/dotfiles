7f5971aaac0561e0c2b1e4a26af48c2a6faf6c07
Julio C. Villasante <jvillasantegomez@gmail.com>
2026-02-12 14:48:50 -0500

    changes changes...

--- Common/emacs/emacs.d/custom.el
+++ Common/emacs/emacs.d/custom.el
@@ -8,6 +8,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ibuffer-formats
+      '((mark modified read-only locked " " (name 30 30 :left :elide) " "
+            (size 9 -1 :right) " " (mode 16 16 :left :elide) " "
+            project-file-relative)))
  '(package-selected-packages
       '(adoc-mode anzu apheleia avy bind-key cape chatgpt-shell cl-generic cl-lib
            cmake-mode compat consult-dir consult-notes corfu crux csv-mode dape
