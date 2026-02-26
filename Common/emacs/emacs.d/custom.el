e514939ef9a5224b353555b6af4637542422ff28
Julio C. Villasante <jvillasantegomez@gmail.com>
2026-02-26 11:25:01 -0500

    emacs stuff

--- Common/emacs/emacs.d/custom.el
+++ Common/emacs/emacs.d/custom.el
@@ -8,29 +8,25 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ibuffer-formats
+      '((mark modified read-only locked " " (name 30 30 :left :elide) " "
+            (size 9 -1 :right) " " (mode 16 16 :left :elide) " "
+            project-file-relative)))
  '(package-selected-packages
-      '(adoc-mode anzu apheleia bind-key cape cl-generic cl-lib claude-code-ide
-           cmake-mode compat consult-dir consult-notes corfu crux csv-mode dape
-           deadgrep devdocs-browser diff-hl dired-sidebar diredfl docker
-           dockerfile-mode dwim-shell-command editorconfig eglot eldoc
-           eldoc-diffstat elfeed elisp-demos embark-consult engine-mode erc
-           expreg external-completion faceup fancy-compilation flash flymake
-           geiser-guile gnuplot go-mode gptel helpful hl-todo ibuffer-project
-           idlwave jinx js2-mode json-mode jsonrpc ledger-mode let-alist
+      '(acp adoc-mode anzu apheleia cape cl-generic claude-code-ide cmake-mode
+           consult-dir consult-notes corfu crux csv-mode dape deadgrep
+           devdocs-browser diff-hl dired-sidebar diredfl docker dockerfile-mode
+           dwim-shell-command editorconfig eglot eldoc-diffstat elfeed
+           elisp-demos embark-consult engine-mode erc expreg faceup
+           fancy-compilation flash geiser-guile gnuplot go-mode gptel helpful
+           hl-todo ibuffer-project idlwave jinx js2-mode json-mode ledger-mode
            lua-mode magit map marginalia markdown-mode minions modus-themes
-           monkeytype multiple-cursors nadvice nov ntlm orderless org
-           org-superstar otpp package-lint-flymake password-store
-           pdf-view-restore peg persistent-scratch php-mode project python
-           rmsbolt rust-mode seq so-long soap-client svg track-changes tramp-hlo
-           trashed use-package verb verilog-mode vertico vim-tab-bar vterm vundo
-           web-mode wgrep which-key window-tool-bar xref yaml-mode
-           yasnippet-snippets zig-mode))
- '(package-vc-selected-packages
-      '((claude-code-ide :url "https://github.com/manzaltu/claude-code-ide.el")
-           (gptel :url "git@github.com:karthink/gptel.git")
-           (deadgrep :url "git@github.com:Wilfred/deadgrep.git")
-           (flash :url "git@github.com:Prgebish/flash.git")
-           (ledger-mode :url "git@github.com:ledger/ledger-mode.git"))))
+           monkeytype multiple-cursors nov ntlm orderless org-superstar otpp
+           package-lint-flymake password-store pdf-view-restore peg
+           persistent-scratch php-mode python rmsbolt rust-mode shell-maker
+           so-long soap-client svg track-changes tramp-hlo trashed use-package
+           verb verilog-mode vertico vim-tab-bar vterm vundo web-mode wgrep
+           which-key window-tool-bar yaml-mode yasnippet-snippets zig-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
