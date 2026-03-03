6d36252a5a5768213db70665e5e8d9fb6ba54fd5
Julio C. Villasante <jvillasantegomez@gmail.com>
2026-03-03 11:16:04 -0500

    Back to agent-shell

--- Common/emacs/elisp/my-init-ai.el
+++ Common/emacs/elisp/my-init-ai.el
@@ -37,7 +37,6 @@
           (password-store-get-field "Work/Omicron/Gemini" "API Key")))))
 
 (use-package agent-shell
-    :disabled t
     :defer t
     :vc (:url "https://github.com/xenodium/agent-shell.git"
               :rev :newest)
@@ -56,6 +55,7 @@
      (agent-shell-anthropic-make-authentication :login t)))
 
 (use-package claude-code-ide
+    :disabled t
     :defer t
     :vc (:url "https://github.com/manzaltu/claude-code-ide.el"
               :rev :newest)
