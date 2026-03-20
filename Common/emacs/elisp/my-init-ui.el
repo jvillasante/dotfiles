5992b9cba80a65045e5de17fb7c34216a920a497
Julio C. Villasante <jvillasantegomez@gmail.com>
2026-03-20 09:19:08 -0400

    Removing ad-hoc scrolling and using `ultra-scroll`

--- Common/emacs/elisp/my-init-ui.el
+++ Common/emacs/elisp/my-init-ui.el
@@ -134,81 +134,30 @@
     :ensure nil ;; emacs built-in
     :hook (after-init . winner-mode))
 
-;; scrolling
-(progn
-    ;; Enables faster scrolling through unfontified regions. This may result in
-    ;; brief periods of inaccurate syntax highlighting immediately after scrolling,
-    ;; which should quickly self-correct.
-    (setq fast-but-imprecise-scrolling t)
-
-    ;; Move point to top/bottom of buffer before signaling a scrolling error.
-    (setq scroll-error-top-bottom t)
-
-    ;; The number of lines to try scrolling a window by when point moves out.
-    (setq scroll-step 1)
-
-    ;; Emacs spends excessive time recentering the screen when the cursor moves more
-    ;; than N lines past the window edges (where N is the value of
-    ;; `scroll-conservatively`). This can be particularly slow in larger files
-    ;; during extensive scrolling. If `scroll-conservatively` is set above 100, the
-    ;; window is never automatically recentered. The default value of 0 triggers
-    ;; recentering too aggressively. Setting it to 10 reduces excessive recentering
-    ;; and only recenters the window when scrolling significantly off-screen.
-    (setq scroll-conservatively 10)
-
-    ;; Try to keep screen position when PgDn/PgUp.
-    (setq scroll-preserve-screen-position t)
-
-    ;; Start scrolling when marker at top/bottom.
-    (setq scroll-margin 1)
-
-    ;; Horizontal scrolling
-    (setq hscroll-margin 2
-          hscroll-step 1)
-
-    ;; Mouse scroll moves 1 line at a time, instead of 5 lines.
-    (setq mouse-wheel-scroll-amount '(1))
-
-    ;; On a long mouse scroll keep scrolling by 1 line.
-    (setq mouse-wheel-progressive-speed nil)
-
-    ;; Speedup cursor movement.
-    ;; https://emacs.stackexchange.com/questions/28736/emacs-pointcursor-movement-lag/28746
-    (setq auto-window-vscroll nil)
-
-    ;; smooth scrolling
-    (use-package pixel-scroll
-        :ensure nil ;; emacs built-in
-        :if (fboundp 'pixel-scroll-precision-mode)
-        :preface
-        (defvar my-default-scroll-lines 25) ;; scroll less than default
-        (defun my-pixel-scroll-up-command (&optional arg)
-            "Similar to `scroll-up-command' but with pixel scrolling."
-            (interactive "^P")
-            (pixel-scroll-precision-interpolate (- (* my-default-scroll-lines (line-pixel-height)))))
-        (defun my-pixel-scroll-down-command (&optional arg)
-            "Similar to `scroll-down-command' but with pixel scrolling."
-            (interactive "^P")
-            (pixel-scroll-precision-interpolate (* my-default-scroll-lines (line-pixel-height))))
-        (defun my-pixel-recenter-top-bottom (&optional arg)
-            "Similar to `recenter-top-bottom' but with pixel scrolling."
-            (interactive "^P")
-            (let* ((current-row (cdr (nth 6 (posn-at-point))))
-                   (target-row (save-window-excursion
-                                   (recenter-top-bottom)
-                                   (cdr (nth 6 (posn-at-point)))))
-                   (distance-in-pixels (* (- target-row current-row) (line-pixel-height))))
-                (pixel-scroll-precision-interpolate distance-in-pixels)))
-        :bind (([remap scroll-up-command]   . my-pixel-scroll-up-command)
-               ([remap scroll-down-command] . my-pixel-scroll-down-command)
-               ([remap recenter-top-bottom] . my-pixel-recenter-top-bottom))
-        :custom ((pixel-scroll-precision-interpolation-factor 0.75)
-                 (pixel-scroll-precision-use-momentum nil)
-                 (pixel-scroll-precision-interpolate-mice t)
-                 (pixel-scroll-precision-large-scroll-height 10.0)
-                 (pixel-scroll-precision-interpolation-total-time 0.2)
-                 (pixel-scroll-precision-interpolate-page t))
-        :hook (after-init . pixel-scroll-precision-mode)))
+;; ultra-scroll : scroll emacs like lightning
+(use-package ultra-scroll
+    :preface
+    (defun my-scroll-half-page-down ()
+        "Scroll down half a screen."
+        (interactive)
+        (let ((half (/ (window-body-height) 2)))
+            (scroll-up half)))
+    (defun my-scroll-half-page-up ()
+        "Scroll up half a screen."
+        (interactive)
+        (let ((half (/ (window-body-height) 2)))
+            (scroll-down half)))
+    :init
+    (setq scroll-conservatively 3 ; or whatever value you prefer, since v0.4
+          scroll-margin 0)        ; important: scroll-margin>0 not yet supported
+    :config
+    (setq ultra-scroll-hide-cursor 0.25)    ; hide cursor at window edges during scroll
+    (setq ultra-scroll-preserve-column t)   ; restore column after scroll completes
+    :hook
+    (after-init . ultra-scroll-mode)
+    :bind
+    ("C-v" . my-scroll-half-page-down)
+    ("M-v" . my-scroll-half-page-up))
 
 ;; display line numbers in the left margin of the window.
 (use-package display-line-numbers
--- Common/emacs/emacs.d/custom.el
+++ Common/emacs/emacs.d/custom.el
@@ -8,28 +8,28 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ibuffer-formats
-      '((mark modified read-only locked " " (name 30 30 :left :elide) " "
-            (size 9 -1 :right) " " (mode 16 16 :left :elide) " "
-            project-file-relative)))
  '(package-selected-packages
-      '(acp adoc-mode agent-shell anzu cape cl-generic claude-code-ide cmake-mode
+      '(adoc-mode agent-shell anzu bind-key cape cl-generic cl-lib cmake-mode compat
            consult-dir consult-notes corfu crux csv-mode dape deadgrep
            devdocs-browser diff-hl dired-sidebar diredfl docker dockerfile-mode
-           editorconfig eglot eldoc-diffstat elfeed elisp-demos embark-consult
-           engine-mode erc expreg faceup fancy-compilation flash geiser-guile
-           gnuplot go-mode helpful hl-todo ibuffer-project idlwave jinx js2-mode
-           json-mode ledger-mode lua-mode magit map marginalia markdown-mode
-           minions modus-themes monkeytype multiple-cursors nov ntlm orderless
-           org-superstar otpp package-lint-flymake password-store
-           pdf-view-restore peg persistent-scratch php-mode python rmsbolt
-           rust-mode shell-maker so-long soap-client surround svg track-changes
-           tramp-hlo trashed use-package verb verilog-mode vertico vim-tab-bar
-           vterm vundo web-mode wgrep which-key window-tool-bar yaml-mode
-           yasnippet-snippets zig-mode))
+           editorconfig eglot eldoc eldoc-diffstat elfeed elisp-demos
+           embark-consult engine-mode erc expreg external-completion faceup
+           flash flymake geiser-guile gnuplot go-mode helpful hl-todo
+           ibuffer-project idlwave jinx js2-mode json-mode jsonrpc ledger-mode
+           let-alist lua-mode magit map marginalia markdown-mode minions
+           modus-themes monkeytype multiple-cursors nadvice nov ntlm orderless
+           org org-superstar otpp package-lint-flymake password-store
+           pdf-view-restore peg persistent-scratch php-mode project python
+           rmsbolt rust-mode seq shell-maker so-long soap-client surround svg
+           track-changes tramp-hlo trashed ultra-scroll use-package verb
+           verilog-mode vertico vim-tab-bar vterm vundo web-mode wgrep which-key
+           window-tool-bar xref yaml-mode yasnippet-snippets zig-mode))
  '(package-vc-selected-packages
       '((agent-shell :url "https://github.com/xenodium/agent-shell.git")
-           (shell-maker :url "https://github.com/xenodium/shell-maker.git"))))
+           (shell-maker :url "https://github.com/xenodium/shell-maker.git")
+           (deadgrep :url "git@github.com:Wilfred/deadgrep.git")
+           (flash :url "git@github.com:Prgebish/flash.git")
+           (ledger-mode :url "git@github.com:ledger/ledger-mode.git"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
