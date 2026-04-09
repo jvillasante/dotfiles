e39c7223713a60cde0ed77c4f48542e217ff5471
Julio C. Villasante <jvillasantegomez@gmail.com>
2026-04-09 15:19:28 -0400

    More emacs mail changes...

--- Common/emacs/lisp/my-init-mail.el
+++ Common/emacs/lisp/my-init-mail.el
@@ -9,6 +9,20 @@
     :ensure nil ;; built-in
     :defer t)
 
+(use-package org-msg
+    :defer t
+    :hook (after-init . org-msg-mode)
+    :config
+    (setq mail-user-agent 'mu4e-user-agent)
+    (setq org-msg-options "html-postamble:nil H:5 num:nil ^:{} toc:nil author:nil email:nil \\n:t"
+        org-msg-startup "hidestars indent inlineimages"
+        org-msg-greeting-fmt "\nHi%s,\n\n"
+        org-msg-greeting-name-limit 3
+        org-msg-default-alternatives '((new             . (text html))
+                                       (reply-to-html   . (text html))
+                                       (reply-to-text   . (text)))
+        org-msg-convert-citation t))
+
 ;; mu4easy : A global minor mode that defines a full working setup for mu4e and mbsync
 (use-package mu4easy
     :defer t
@@ -17,7 +31,7 @@
                             (mu4e-alert-disable-mode-line-display)))
     :bind ("C-c o u" . mu4e)
     :custom
-    (mu4easy-signature "\n\n*Julio Villasante*")
+    (mu4easy-signature "---\nRegards,\nJulio")
     (mu4easy-headers
         '((:human-date . 18) (:flags . 6) (:maildir . 28) (:from-or-to . 22)
              (:mailing-list . 10) (:tags . 10) (:subject . 92)))
