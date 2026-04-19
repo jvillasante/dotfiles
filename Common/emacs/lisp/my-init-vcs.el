f50d4a8dd274cd27d5b48f68f3b446ee04339028
Julio C. Villasante <jvillasantegomez@gmail.com>
2026-04-19 06:54:27 -0400

    small change...

--- Common/emacs/lisp/my-init-vcs.el
+++ Common/emacs/lisp/my-init-vcs.el
@@ -5,49 +5,50 @@
 
 ;; nntp worktree helper - create a worktree and seed it with personal config
 ;; files that are not tracked by git.
-(defvar my/nntp-worktree-base (expand-file-name "Projects/nntp" my/work-path)
-    "Parent directory where nntp worktrees live.")
+(progn
+    (defvar my/nntp-worktree-base (expand-file-name "Projects/nntp" my/work-path)
+        "Parent directory where nntp worktrees live.")
 
-(defvar my/nntp-dotfiles-dir (expand-file-name "Misc/work/nntp" my/dotfiles-path)
-    "Directory containing personal config files to copy into new worktrees.")
+    (defvar my/nntp-dotfiles-dir (expand-file-name "Misc/work/nntp" my/dotfiles-path)
+        "Directory containing personal config files to copy into new worktrees.")
 
-(defvar my/nntp-config-files '((".clang-tidy"               . ".clang-tidy")
-                                  (".dir-locals.el"           . ".dir-locals.el")
-                                  ("compile_flags.fedora.txt" . "compile_flags.txt")
-                                  (".scripts"                 . ".scripts"))
-    "Alist of (SOURCE . DEST) for symlinks from dotfiles into each new worktree.
+    (defvar my/nntp-config-files '((".clang-tidy"               . ".clang-tidy")
+                                      (".dir-locals.el"           . ".dir-locals.el")
+                                      ("compile_flags.fedora.txt" . "compile_flags.txt")
+                                      (".scripts"                 . ".scripts"))
+        "Alist of (SOURCE . DEST) for symlinks from dotfiles into each new worktree.
 SOURCE is relative to `my/nntp-dotfiles-dir', DEST is relative to the worktree.")
 
-(defun my/nntp-worktree-add (name start-point &optional detach)
-    "Create an nntp worktree called NAME based on START-POINT.
+    (defun my/nntp-worktree-add (name start-point &optional detach)
+        "Create an nntp worktree called NAME based on START-POINT.
 With prefix argument DETACH, use detached HEAD (useful for code reviews)."
-    (interactive
-        (let ((n (read-string "Worktree name: "))
-                 (sp (magit-read-branch-or-commit "Starting point")))
-            (list n sp current-prefix-arg)))
-    (let* ((base my/nntp-worktree-base)
-              (master (expand-file-name "master" base))
-              (wt-path (expand-file-name name base))
-              (dotfiles my/nntp-dotfiles-dir)
-              (args (if detach
-                        (list "worktree" "add" wt-path start-point)           ;; detached HEAD for reviews
-                        (list "worktree" "add" "-b" name wt-path start-point)))) ;; new branch for development
-        (when (file-exists-p wt-path)
-            (user-error "Worktree %s already exists" wt-path))
-        ;; Create the worktree
-        (let ((default-directory master))
-            (apply #'magit-run-git args)
-            (message "Created worktree at %s" wt-path))
-        ;; Set upstream for easy rebasing (only for branches, not detached)
-        (unless detach
-            (let ((default-directory wt-path))
-                (magit-run-git "branch" (format "--set-upstream-to=%s" start-point) name)))
-        ;; Symlink personal config files
-        (dolist (entry my/nntp-config-files)
-            (let ((src (expand-file-name (car entry) dotfiles))
-                     (dst (expand-file-name (cdr entry) wt-path)))
-                (make-symbolic-link src dst)))
-        (message "Symlinked config files into %s" wt-path)))
+        (interactive
+            (let ((n (read-string "Worktree name: "))
+                     (sp (magit-read-branch-or-commit "Starting point")))
+                (list n sp current-prefix-arg)))
+        (let* ((base my/nntp-worktree-base)
+                  (master (expand-file-name "master" base))
+                  (wt-path (expand-file-name name base))
+                  (dotfiles my/nntp-dotfiles-dir)
+                  (args (if detach
+                            (list "worktree" "add" wt-path start-point)           ;; detached HEAD for reviews
+                            (list "worktree" "add" "-b" name wt-path start-point)))) ;; new branch for development
+            (when (file-exists-p wt-path)
+                (user-error "Worktree %s already exists" wt-path))
+            ;; Create the worktree
+            (let ((default-directory master))
+                (apply #'magit-run-git args)
+                (message "Created worktree at %s" wt-path))
+            ;; Set upstream for easy rebasing (only for branches, not detached)
+            (unless detach
+                (let ((default-directory wt-path))
+                    (magit-run-git "branch" (format "--set-upstream-to=%s" start-point) name)))
+            ;; Symlink personal config files
+            (dolist (entry my/nntp-config-files)
+                (let ((src (expand-file-name (car entry) dotfiles))
+                         (dst (expand-file-name (cdr entry) wt-path)))
+                    (make-symbolic-link src dst)))
+            (message "Symlinked config files into %s" wt-path))))
 
 ;; vc : builtin emacs version control
 (use-package vc
