4935c198325d211818730ffd260806fe11da8237
Julio C. Villasante <jvillasantegomez@gmail.com>
2026-04-23 08:15:14 -0400

    small change...

--- Common/emacs/emacs.d/custom.el
+++ Common/emacs/emacs.d/custom.el
@@ -14,7 +14,7 @@
            corfu-prescient crux csv-mode dape devdocs-browser diff-hl
            dired-sidebar diredfl dockerfile-mode eldoc-diffstat elfeed
            elisp-demos embark-consult engine-mode expreg flash geiser-guile
-           ghostel gnuplot go-mode helpful hl-todo ibuffer-project jinx js2-mode
+           gnuplot go-mode helpful hl-todo ibuffer-project jinx js2-mode
            json-mode magit marginalia markdown-mode minions monkeytype mu4easy
            multiple-cursors nov orderless org-superstar otpp
            package-lint-flymake password-store pdf-view-restore
@@ -24,3 +24,9 @@
 
 (provide 'custom)
 ;;; custom.el ends here
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
