3ed2771fe120e5927bf11de6c72e968a427c7dd0
Julio C. Villasante <jvillasantegomez@gmail.com>
2026-05-14 12:23:00 -0400

    Better XDG Directories

--- Common/shell/post_install/fedora.sh
+++ Common/shell/post_install/fedora.sh
@@ -119,21 +119,22 @@ fedora_install() {
                     [ -z "$HOSTNAME_STATIC" ] && HOSTNAME_STATIC="fedora-xps-9710"
                     sudo hostnamectl set-hostname --static "$HOSTNAME_STATIC"
 
-                    # xdg
+                    # XDG
                     export XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
                     export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
                     export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
-                    export XDG_LIB_HOME="${XDG_LIB_HOME:-$HOME/.local/lib}"
                     export XDG_STATE_HOME="${XDG_STATE_HOME:-$HOME/.local/state}"
+                    export XDG_BIN_HOME="${XDG_BIN_HOME:-$HOME/.local/bin}"
+                    export XDG_LIB_HOME="${XDG_LIB_HOME:-$HOME/.local/lib}"
                     mkdir -p \
                           "${XDG_CACHE_HOME}" \
                           "${XDG_CONFIG_HOME}" \
                           "${XDG_DATA_HOME}" \
-                          "${XDG_LIB_HOME}" \
-                          "${XDG_STATE_HOME}"
+                          "${XDG_STATE_HOME}" \
+                          "${XDG_BIN_HOME}" \
+                          "${XDG_LIB_HOME}"
 
                     # Folder structure
-                    mkdir -p "$HOME/.local/bin"
                     mkdir -p "$HOME/Workspace"
                     mkdir -p "$HOME/Workspace/Books"
                     mkdir -p "$HOME/Workspace/Private/Projects"
