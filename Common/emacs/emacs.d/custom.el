839d872bf8d1033e2eb10bc528373b13837c2480
Julio C. Villasante <jvillasantegomez@gmail.com>
2026-06-14 07:43:25 -0400

    new emacs compile

--- Common/emacs/emacs.d/custom.el
+++ Common/emacs/emacs.d/custom.el
@@ -8,9 +8,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(c-ts-indent-offset 4 nil nil "Customized with use-package c++-ts-mode")
  '(package-selected-packages
-      '(activities adoc-mode agent-recall bash-completion cape cmake-mode consult-dir
+      '(activities adoc-mode agent-shell bash-completion cape cmake-mode consult-dir
            consult-notes corfu crux csv-mode dape devdocs-browser diff-hl
            dired-sidebar diredfl dockerfile-mode elfeed elisp-demos
            embark-consult engine-mode expreg flash geiser-guile ghostel gnuplot
@@ -19,8 +18,7 @@
            multiple-cursors nov orderless org-superstar package-lint-flymake
            password-store pcmpl-args pdf-view-restore persistent-scratch
            php-mode rmsbolt rust-mode surround trashed verb vertico vim-tab-bar
-           vundo web-mode web-server websocket yaml-mode yasnippet-snippets
-           zig-mode)))
+           vundo web-mode yaml-mode yasnippet-snippets zig-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
