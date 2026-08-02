4d7251a4eeb538406eba7b63c978bd5ca0dc73f9
Julio C. Villasante <jvillasantegomez@gmail.com>
2026-08-02 15:54:21 -0400

    `my/smart-kill-buffer` also deletes the window

--- Common/emacs/lisp/my-init-bindings.el
+++ Common/emacs/lisp/my-init-bindings.el
@@ -21,12 +21,21 @@
 (defun my/smart-kill-buffer ()
     "Kill buffers.
 When run interactively:
-- Without a prefix arg (`C-x C-k`), `kill-current-buffer'.
-- With a prefix arg (`C-u C-x C-k`), `kill-buffer'."
+- Without a prefix arg (`C-x k`), `kill-current-buffer'.
+- With a prefix arg (`C-u C-x k`), `kill-buffer'.
+Also deletes the window if there are multiple windows open."
     (interactive)
-    (if current-prefix-arg
-        (call-interactively #'kill-buffer)
-        (kill-current-buffer)))
+    (let* ((buf (current-buffer))
+              ;; True if there is more than one window in the frame
+              (should-close (not (one-window-p))))
+
+        (if current-prefix-arg
+            (call-interactively #'kill-buffer)
+            (kill-current-buffer))
+
+        ;; If the buffer is dead and there was a split, close the split
+        (when (and should-close (not (buffer-live-p buf)))
+            (delete-window))))
 (global-set-key (kbd "C-x k") 'my/smart-kill-buffer)
 
 ;; smart kill emacs
